c523990d31fce4042c8165192ab952fac49c710a
diegonobu <diegonobu@gmail.com>
2014-05-06 11:29:23 -0400

    jogador não ultrapassa mais as laterais

--- felipenoir/breakoutObject/init.js
+++ felipenoir/breakoutObject/init.js
@@ -2,101 +2,102 @@
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-	
-var janela = new Janela(ctx,canvas.width,canvas.height);
 
-geraPosicaoRandom = function(width){
+var janela = new Janela(ctx, canvas.width, canvas.height);
+
+geraPosicaoRandom = function(width) {
 	var valor = 1;
-	while(true){
-		valor = Math.floor((Math.random()*width) +1);
-		if(valor >=0 && valor <= width)
+	while (true) {
+		valor = Math.floor((Math.random() * width) + 1);
+		if (valor >= 0 && valor <= width)
 			break;
 	}
 	return valor;
 }
-//Gera posicai radom para bola
+// Gera posicai radom para bola
 var posicaoXBola = geraPosicaoRandom(canvas.width);
-var bola = new Bola(ctx,posicaoXBola,canvas.width,canvas.height);
-var jogador = new Jogador(ctx,canvas.width,canvas.height);
-var inimigos = new Inimigos(ctx,canvas.width,canvas.height);
+var bola = new Bola(ctx, posicaoXBola, canvas.width, canvas.height);
+var jogador = new Jogador(ctx, canvas.width, canvas.height);
+var inimigos = new Inimigos(ctx, canvas.width, canvas.height);
 
 var isDireita = false;
 var isEsquerda = false;
 
-//Usuário movendo
+// Usuário movendo
 function onKeyDown(evt) {
-  if (evt.keyCode == 39) isDireita = true;
-  else if (evt.keyCode == 37) isEsquerda = true;
+	if (evt.keyCode == 39)
+		isDireita = true;
+	else if (evt.keyCode == 37)
+		isEsquerda = true;
 }
 
-//usuário parado
+// usuário parado
 function onKeyUp(evt) {
-  if (evt.keyCode == 39) isDireita = false;
-  else if (evt.keyCode == 37) isEsquerda = false;
+	if (evt.keyCode == 39)
+		isDireita = false;
+	else if (evt.keyCode == 37)
+		isEsquerda = false;
 }
 
-document.addEventListener('keyup',onKeyUp,false);
-document.addEventListener('keydown',onKeyDown,false);
+document.addEventListener('keyup', onKeyUp, false);
+document.addEventListener('keydown', onKeyDown, false);
 
-function init(){
-	try{
+function init() {
+	try {
 		setInterval(gameLoop, 10)
-	}catch(e){
+	} catch (e) {
 		console.log("TRETA:" + e.message)
 	}
 }
 
-function gameLoop(){
+function gameLoop() {
+	bola.movimentaBola(janela.width, janela.height);
+	jogador.movimentaJogador(isDireita, isEsquerda, canvas.width);
+
+	// COLISAO
+	verificarColisao(bola, jogador);
+	verificaColisao(bola, inimigos);
+
 	janela.desenhaJanela();
-	//BOLA
 	bola.desenhaBola(10);
-	bola.movimentaBola(janela.width,janela.height);
-	
-	//JOGADOR
 	jogador.desenhaJogador()
-	jogador.movimentaJogador(isDireita,isEsquerda);
-	
-	//INIMIGOS
 	inimigos.desenhaInimigos();
-	
-	//COLISAO
-	verificarColisao(bola,jogador);
-	verificaColisao(bola,inimigos);
-	
-	
+
 }
 
-function verificarColisao(bola,jogador){
-	if(bola.posX + bola.raio > canvas.width || bola.posX < 0)
-		bola.velocidadeX = - bola.velocidadeX
-	
-	if(bola.posY + bola.raio < 0)
-		bola.velocidadeY = - bola.velocidadeY;
-	else if(bola.posY + bola.raio > canvas.height - jogador.height){
-		if(bola.posX > jogador.posX && bola.posX < jogador.posX + jogador.width){
+function verificarColisao(bola, jogador) {
+	if (bola.posX + bola.raio > canvas.width || bola.posX < 0)
+		bola.velocidadeX = -bola.velocidadeX
+
+	if (bola.posY + bola.raio < 0)
+		bola.velocidadeY = -bola.velocidadeY;
+	else if (bola.posY + bola.raio > canvas.height - jogador.height) {
+		if (bola.posX > jogador.posX
+				&& bola.posX < jogador.posX + jogador.width) {
 			bola.velocidadeY = -bola.velocidadeY;
-		}else{
+		} else {
 			gameOver();
 		}
 	}
 }
 
-function verificaColisao(bola,inimigos){
+function verificaColisao(bola, inimigos) {
 	var heightTotal = inimigos.height + inimigos.DISTANCIA;
 	var widthTotal = inimigos.width + inimigos.DISTANCIA;
-	var linha = Math.floor(bola.posY/heightTotal);
-	var coluna = Math.floor(bola.posX/widthTotal);
-	if(bola.posY < inimigos.QTDCOLUNAS*heightTotal  && linha >= 0 && coluna >= 0 && inimigos.inimigos[linha][coluna] == 1){
-		//Ouve uma colisão
+	var linha = Math.floor(bola.posY / heightTotal);
+	var coluna = Math.floor(bola.posX / widthTotal);
+	if (bola.posY < inimigos.QTDCOLUNAS * heightTotal && linha >= 0
+			&& coluna >= 0 && inimigos.inimigos[linha][coluna] == 1) {
+		// Ouve uma colisão
 		bola.velocidadeY = -bola.velocidadeY;
 		inimigos.inimigos[linha][coluna] = 0;
 	}
-	
+
 }
 
-function gameOver(){
+function gameOver() {
 	clearInterval(0);
 }
 
-//The Treta has been planted
+// The Treta has been planted
 init();
