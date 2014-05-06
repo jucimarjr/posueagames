47f9e5fda35f14c2cc10e4a9a75952959a0d239a
Raymundo Junior <ext-raymundojunior@nokia.com>
2014-05-06 10:09:50 -0400

    Organizacao do codigo e adicao de comentarios

--- brunorichele/breakout/script/tecla.js
+++ brunorichele/breakout/script/tecla.js
@@ -1,30 +1,45 @@
+/**
+ * @class tecla
+ * @description Classe responsável por implementar os métodos e escutar eventos do teclado
+ * @author Bruno Araujo <bruno.araujo@gmail.com>, Bruno Richele <bruno.richele@gmail.com>, Cristina Araujo <crisoara@gmail.com>, Raymundo Junior <raymundojunior@gmail.com> 
+ * @version 1.0 
+ */
 var tecla = {
     direita: false,
 	esquerda: false,
-	
+	key : {
+		left  : 37, 
+		up    : 38, 
+		right : 39, 
+		down  : 40
+	},
+	/**
+	*@description Método responsável por iniciar a captura dos eventos do teclado 
+	*/
 	listenerKey : function()
 	{
-        document.addEventListener('keyup', tecla.keyUp, false);		
-		document.addEventListener('keydown', tecla.keyDown, false);
+		document.addEventListener('keydown', tecla.keyDown, false); //Ativa quando a tecla é pressionada 
+        document.addEventListener('keyup', tecla.keyUp, false);	//Ativa quando a tecla é liberada 	
 	},
-
-	// Códigos de teclas:
-	// Esquerda: 37
-	// Direita: 39
-	 keyDown : function(e){
-		if(e.keyCode == 37){
+	/**
+	*@description Método que trata a tecla pressionada 
+	*/	
+	keyDown : function(e){
+		if(e.keyCode == tecla.key.left){
 			tecla.esquerda = true;
 		}
-		else if(e.keyCode == 39){ 
+		else if(e.keyCode == tecla.key.right){ 
 			tecla.direita = true;
 		}
 	},
-
-	 keyUp : function(e){
-		if(e.keyCode == 37){
+	/**
+	*@description Método que trata a tecla quando liberada
+	*/	
+	keyUp : function(e){
+		if(e.keyCode == tecla.key.left){
 			tecla.esquerda = false;
 		}
-		else if(e.keyCode == 39){
+		else if(e.keyCode == tecla.key.right){
 			tecla.direita = false;
 		}
 	}	
