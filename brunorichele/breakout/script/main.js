0b3dbf2ee90db776878eb81a64835644d5ecafb2
Raymundo Junior <ext-raymundojunior@nokia.com>
2014-05-07 10:09:51 -0400

    add menu do jogo

--- brunorichele/breakout/script/main.js
+++ brunorichele/breakout/script/main.js
@@ -1,3 +1,19 @@
 window.onload=function(){
-	game.init();
+	main.init();
+};
+
+var main = {
+	init : function(){
+		main.bind();
+	},
+	bind : function(){
+		document.getElementById("jogar").onclick = function(){
+			main.jogar();
+		}	
+	},
+	jogar : function(){
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("canvas").style.display = "block";
+		game.init();
+	}
 };
