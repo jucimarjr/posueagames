db6bc883f5a3ed60f577dd114f01c0a9c149a1f0
glmiyamoto <glmiyamoto@gmail.com>
2014-05-07 14:41:38 -0300

    Adjustado colisao basica da bola

--- fcumaru/breakout/breakout.js
+++ fcumaru/breakout/breakout.js
@@ -5,6 +5,8 @@ var player, ball, blocks, background;
 
 var keyLeft, keyRight;
 
+var bolaParaBaixo = false;
+
 function init() {
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
@@ -14,7 +16,7 @@ function init() {
 
 	background = new background();
 	player = new player(width / 2, height, 100, 10, 10);
-	ball = new ball(width / 2, height / 2, 5, 15);
+	ball = new ball(width / 2, height / 2, 5, 10);
 
 	blocks = new Array();
 	for (var j = 100; j < 180; j += 20) {
@@ -25,7 +27,7 @@ function init() {
 
 	keyRight = false;
 	keyLeft = false;
-
+	
 	document.addEventListener('keyup', keyUp, false);
 	document.addEventListener('keydown', keyDown, false);
 	setInterval(gameLoop, 30);
@@ -99,21 +101,27 @@ function ball(x, y, radius, speed) {
 	};
 
 	this.update = function(player) {
-		if ((this.y - this.radius) <= (player.y + player.h)) {
-			if ((this.x + this.radius > player.x)
-					&& (this.x - this.radius < player.x + player.w)) {
+		// se a bola enconstar na jogador(eixo Y)...
+		if ((this.y + this.radius) > player.y) {
+			// se a bola enconstar na jogador(eixo X)...
+			if ((this.x + this.radius > player.x) && (this.x - this.radius < player.x + player.w)) {
 				bolaParaBaixo = true;
-				if (keyLeft) {
-					angle = Math.floor(Math.random() * 10) - 9;
-				} else {
-					angle = Math.floor((Math.random() * 10));
+				if (keyLeft) {// se o jogador estiver indo para esquerda quando tocar na bola...
+					angle = Math.floor(Math.random() * 10) - 9;// mandamos a bola na diagonal pra esquerda
+				} else {// se o jogador estiver indo para direita quando tocar na bola...
+					angle = Math.floor((Math.random() * 10));// mandamos a bola na diagonal pra direita
 				}
-			} else {
-				
 			}
+		} else if ((this.y + this.radius) < 0) {
+			bolaParaBaixo = false;
 		}
+        
+        if ((this.x - this.radius <= 0) || (this.x + radius > width)) {// se a bola bater na lateral da tela...
+        	this.angle = this.angle * -1;// multiplicamos por -1 para inverter o sinal e a direção da bola no eixo X
+        }
+        this.x += this.angle;// movemos a bola para cima ou para baixo, de acordo com o cáculo acima
 
-		if (bolaParaBaixo) {
+		if (!bolaParaBaixo) {
 			this.y += this.speed;
 		} else {
 			this.y -= this.speed;
