59ad4f1b3c6e3e7f2491536bcedab86a67cef692
esashika <rhedson@gmail.com>
2014-05-14 18:21:00 -0400

    - colocando o som para funcionar

--- yasminetkn/breakout/init.js
+++ yasminetkn/breakout/init.js
@@ -1,15 +1,15 @@
 
-	
-	
-   
-	
+
+
+
+
     var canvas, context,
     barraWidth, barraHeight,
     jogadorPosX, jogadorPosY,bolaRaio, bolaPosX, bolaPosY,
     pontosJogador, pontosOponente,teclaCimaPressionada, teclaBaixoPressionada, oponenteParaCima, velocidadeOponente,
-    bolaRaio, bolaParaDireita, bolaAngulo,velocidadeBola,teclaEsquerdaPressionada,teclaDireitaPressionada, 
+    bolaRaio, bolaParaDireita, bolaAngulo,velocidadeBola,teclaEsquerdaPressionada,teclaDireitaPressionada,
     jogador, bola, mapaTeclas, estadoBola, tempo, tijolo, tijolos, score =0;
-    
+
     mapaTecla = new Array();
     velocidadeJogador = 20;
     window.onload = init;
@@ -21,6 +21,13 @@
     tempo = 0;
     bola = new Bola(200, 560, 7);
     var tijolos = new Array();
+
+    var bouncingSound = new Audio("sound/bounce.ogg");
+    var breakingSound = new Audio("sound/break.ogg");
+    var starterSound = new Audio("sound/skratch.wav");
+    var gameOver = new Audio("sound/skratch17.wav");
+    var zap = new Audio("sound/zap.wav");
+
 	function init(){
 
 		canvas = document.getElementById("canvas");// procura o canvas
@@ -29,61 +36,61 @@
         jogador.draw();
 		pontosJogador = 0;
 
-		criadorDeBlocos(); 
-
+		criadorDeBlocos();
+        starterSound.play();
         setInterval(gameLoop, 30);// chama a function gameLoop a cada 30 frames
 	}
-	
-	
+
+
 	function teclaPressionada(tecla){
 		mapaTecla[tecla.keyCode] = true;
 	}
-	
+
 	function teclaSolta(tecla){
-		mapaTecla[tecla.keyCode] = false;	
+		mapaTecla[tecla.keyCode] = false;
 	}
-	
+
 	function paint(){
 		 context.clearRect(0, 0, canvas.width, canvas.height);// limpa a tela antes de desenhar
 		 jogador.draw();
 		 bola.draw();
-		 criadorDeBlocos(); 
+		 criadorDeBlocos();
 	}
-	
+
 	//Funcao pra gerar um valor de limite
 	function clamp(val, min, max){
 		return Math.max(min, Math.min(max,val));
 	}
-	
+
 	function detectarColisaoJogadorEBola(){
 		var xMaisProximo = clamp(bola.x, jogador.x, (jogador.x+jogador.largura));
 		var yMaisProximo = clamp(bola.y, jogador.y, (jogador.y+jogador.altura));
-		
+
 		var distanciaX = bola.x - xMaisProximo;
 		var distanciaY = bola.y - yMaisProximo;
 		var distancia = (distanciaX*distanciaX)+(distanciaY*distanciaY);
-		
+
 		return distancia < (bola.raio*bola.raio);
 	}
 
 	function gameLoop() {
-		
+
 		if(mapaTecla[37] == true){
-			
+
 			if(jogador.x>0){
 				jogador.mexer(-velocidadeJogador);
 			}
-			
+
 		}else if (mapaTecla[39] == true){
 			if(jogador.x < (canvas.width - tamanhoBarra)){
 				jogador.mexer(velocidadeJogador);
             }
-			
+
 		}
-		
+
 		bola.mover();
 		bola.verificaColisao();
 
-		paint();	
+		paint();
 
     }
