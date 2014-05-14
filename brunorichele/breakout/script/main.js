5196efe1bb74dc513835ca254c22c99251da3a37
Raymundo Junior <ext-raymundojunior@nokia.com>
2014-05-14 18:22:29 -0400

    criando metodos do menu

--- brunorichele/breakout/script/main.js
+++ brunorichele/breakout/script/main.js
@@ -10,10 +10,24 @@ var main = {
 		document.getElementById("jogar").onclick = function(){
 			main.jogar();
 		}
+		document.getElementById("credito").onclick = function(){
+			main.creditos();
+		}
+		document.getElementById("inicio").onclick = function(){
+			main.inicio();
+		}
 	},
 	jogar : function(){
 		document.getElementById("menu").style.display = "none";
 		document.getElementById("canvas").style.display = "block";
 		game.init();
+	},
+	creditos : function(){
+		document.getElementById("links").style.display = "none";
+		document.getElementById("creditos").style.display = "block";	
+	},
+	inicio : function(){
+		document.getElementById("creditos").style.display = "none";
+		document.getElementById("links").style.display = "block";	
 	}
 };
