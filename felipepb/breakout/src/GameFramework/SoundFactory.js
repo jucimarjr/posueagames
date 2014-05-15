8d7e07ea3c6394e8943580178a7f8549ccb11278
Heitor Carlos <htr.carlos@gmail.com>
2014-05-14 20:57:45 -0400

    Renaming loadSound function to receive a format (enum of MP3, Ogg, Wav) rather than a type (string).

--- felipepb/breakout/src/GameFramework/SoundFactory.js
+++ felipepb/breakout/src/GameFramework/SoundFactory.js
@@ -1,7 +1,7 @@
 GameFramework.SoundFactory = {
     loadedSounds: { },
     
-    loadSound: function (sound, type) {
+    loadSound: function (sound, format) {
         var loadedSounds = GameFramework.SoundFactory.loadedSounds;
         var audio = loadedSounds[sound];
 
@@ -12,19 +12,27 @@ GameFramework.SoundFactory = {
             audioElement.preload = 'auto';
             //audioElement.autoplay = 'autoplay';
             sourceElement.src = sound;
-            sourceElement.type = type;
+            sourceElement.type = GameFramework.SoundFactory.MIME[format];
     
             audioElement.appendChild(sourceElement);
             document.body.appendChild(audioElement);
-            
-            //audio = new Audio();
-            //audio.src = audioElement.src === '' ? audioElement.currentSrc : audioElement.src;
-            //audio.load();
 
             audio = new GameFramework.Sound(audioElement);
             loadedSounds[sound] = audio;
         }
 
         return audio;
-    }
+    },
+    
+    Format: {
+        MP3: 0,
+        Ogg: 1,
+        Wav: 2
+    },
+    
+    MIME: { }
 };
+
+GameFramework.SoundFactory.MIME[GameFramework.SoundFactory.Format.MP3] = 'audio/mpeg';
+GameFramework.SoundFactory.MIME[GameFramework.SoundFactory.Format.Ogg] = 'audio/ogg';
+GameFramework.SoundFactory.MIME[GameFramework.SoundFactory.Format.Wav] = 'audio/wav';
