729529d61a68fd44fb63b756803a85bce3737c19
Heitor Carlos <ext-heitor.repolho@nokia.com>
2014-05-16 09:35:24 -0400

    Loads correct sound file (mp3 or wav) for browser.

--- felipepb/breakout/src/MonitorBeat.js
+++ felipepb/breakout/src/MonitorBeat.js
@@ -5,8 +5,13 @@ BreakoutGame.MonitorBeat = {
     play: function() {
         var monitor = BreakoutGame.MonitorBeat._monitor;
         if (monitor === null) {
-            monitor = GameFramework.SoundFactory.loadSound('sounds/monitorbeat.wav',
-                                                           GameFramework.SoundFactory.Format.Wav);
+            if (GameFramework.isInternetExplorer()) {
+                monitor = GameFramework.SoundFactory.loadSound('sounds/monitorbeat.mp3',
+                                                               GameFramework.SoundFactory.Format.MP3);;
+            } else {
+                monitor = GameFramework.SoundFactory.loadSound('sounds/monitorbeat.wav',
+                                                               GameFramework.SoundFactory.Format.Wav);
+            }
             BreakoutGame.MonitorBeat._monitor = monitor;
         }
         monitor.play(0.3);
@@ -15,8 +20,13 @@ BreakoutGame.MonitorBeat = {
     dying: function() {
         var dying = BreakoutGame.MonitorBeat._dying;
         if (dying === null) {
-            dying = GameFramework.SoundFactory.loadSound('sounds/dying.wav',
-                                                         GameFramework.SoundFactory.Format.Wav);
+            if (GameFramework.isInternetExplorer()) {
+                dying = GameFramework.SoundFactory.loadSound('sounds/dying.mp3',
+                                                             GameFramework.SoundFactory.Format.MP3);
+            } else {
+                dying = GameFramework.SoundFactory.loadSound('sounds/dying.wav',
+                                                             GameFramework.SoundFactory.Format.Wav);
+            }
             BreakoutGame.MonitorBeat._dying = dying;
         }
         dying.play();
