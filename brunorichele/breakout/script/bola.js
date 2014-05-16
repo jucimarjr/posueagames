a0da05cf5df24b8d303b2373617891115944eeb7
Raymundo Junior <ext-raymundojunior@nokia.com>
2014-05-16 16:10:19 -0400

    ajustes colisao bloco

--- brunorichele/breakout/script/bola.js
+++ brunorichele/breakout/script/bola.js
@@ -69,7 +69,7 @@ var bola = {
 	},
 	colisaoBloco : function(polling){
 		//Colisao bloco
-		if(bola.y - bola.raio <= (bloco.h * bloco.numLinhas) + bloco.h && bola.y >= 0){
+		if(bola.y - bola.raio <= (bloco.h * bloco.numLinhas) + (bloco.h * 2) && bola.y >= 0){
 			var linha  = Math.floor(((bola.y) - bloco.h) / bloco.h) - 1;
 			var coluna = Math.floor((bola.x) / bloco.w);
 			console.log("linha: " + linha + "coluna:" + coluna);
