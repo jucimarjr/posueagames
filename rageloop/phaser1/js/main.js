b5ea8e18317040450568b23bcf1c7da97d9a86a0
Rodrigo Braga <rodrigogrow@gmail.com>
2014-05-17 10:24:58 -0400

    creating game with phaser

---
+++ rageloop/phaser1/js/main.js
@@ -0,0 +1,21 @@
+(function (app_container) {
+
+	var init = function() {
+
+		// Initialize Phaser, and creates a game
+		app_container.game = new Phaser.Game(600, 960, Phaser.AUTO, 'game_container');
+		
+		// add phaser states
+		app_container.game.state.add('Preload', app_container.Preload);
+		app_container.game.state.add('Menu', app_container.Menu);
+		app_container.game.state.add('Gameplay', app_container.Gameplay);
+
+		// start preload
+		app_container.game.state.start('Preload');
+	};
+
+	window.onload = function() {
+		init();
+	};
+
+}(window.app_container = window.app_container || {}));
