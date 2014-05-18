65d5a73bc92a0edf52810e078a898987645cc7a2
crisoara <crisoara@gmail.com>
2014-05-18 17:15:09 -0400

    alterando resoluçao do game

--- brunorichele/flappybird/js/main.js
+++ brunorichele/flappybird/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< 5028bb97c8c58edbfd251d22a2c0143d7a146070
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: FlappyBird.preload, create: FlappyBird.create, update: FlappyBird.update });
-=======
->>>>>>> 7284205f5460177f330ec78aa619d741d2289e07
+
+var game = new Phaser.Game(960, 600, Phaser.AUTO, '', { preload: FlappyBird.preload, create: FlappyBird.create, update: FlappyBird.update });
+
