802f0349ae569bfecc3a8dea1f8488ebfd39d548
diegonobu <diegonobu@gmail.com>
2014-05-18 02:38:15 -0400

    barreiras

--- felipenoir/flappybirdlike/js/Level.js
+++ felipenoir/flappybirdlike/js/Level.js
@@ -2,6 +2,7 @@ Level = function(game) {
 	this.game = game;
 	this.ground;
 	this.barriers;
+	this.barrierGenerator;
 }
 
 Level.prototype = {
@@ -9,17 +10,38 @@ Level.prototype = {
 		console.log('level -> preload');
 		this.game.load.image('bg', 'assets/bg.png');
 		this.game.load.image('ground', 'assets/ground.png');
+		this.game.load.image('tallbarrier', 'assets/tallbarrier.png');
+		this.game.load.image('shortbarrier', 'assets/shortbarrier.png');
 	},
 
 	create : function() {
 		console.log('level -> create');
+
+		// background
 		this.game.add.sprite(0, 0, 'bg');
+
+		// ground
 		this.ground = this.game.add.tileSprite(0, this.game.world.height - 90,
 				this.game.world.width, 90, 'ground');
 		this.ground.autoScroll(-200, 0);
+
+		// barriers
+		this.barrierGenerator = this.game.time.events.loop(
+				Phaser.Timer.SECOND * 1.25, this.generateBarrier, this);
+		this.barrierGenerator.timer.start();
 	},
 
 	update : function() {
 
+	},
+
+	generateBarrier : function() {
+		console.log('level -> generateBarrier');
+		var barrier = this.game.add.sprite(this.game.world.width,
+				this.game.world.height - 300, 'tallbarrier');
+		this.game.physics.arcade.enableBody(barrier);
+		barrier.body.allowGravity = false;
+		barrier.body.immovable = true;
+		barrier.body.velocity.x = -200;
 	}
 }
