893d0603542560ce1f33407cc4841c4dced505f0
Raymundo Junior <ext-raymundojunior@nokia.com>
2014-05-18 17:52:00 -0400

    adicionando velocidade e escutando o evento da tecla space

--- brunorichele/flappybird/js/create.js
+++ brunorichele/flappybird/js/create.js
@@ -1,10 +1,19 @@
 var create = {
+	space_key : null,
 	init : function(){
+		create.keyboard();
  		create.pirarucu();
 	},
 	pirarucu : function(){
 		pirarucu = game.add.sprite(100, 100, 'pirarucu');
 		game.physics.enable(pirarucu, Phaser.Physics.ARCADE);
 		pirarucu.body.gravity.y = 1000;		
-	}
+	},
+	keyboard : function(){
+        create.space_key = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        create.space_key.onDown.add(create.jump, this);    
+	},
+    jump: function() {
+        pirarucu.body.velocity.y = -200;
+    },	
 };
