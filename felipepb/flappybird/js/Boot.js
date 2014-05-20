d7fcb68cccbd2d7a276298a2adcd74c0e941b7c3
Paulo Mendonca <paulo.rdbm@gmail.com>
2014-05-20 19:10:51 -0400

    Added basic obstacle class with simple pipe placement.

--- felipepb/flappybird/js/Boot.js
+++ felipepb/flappybird/js/Boot.js
@@ -1,12 +1,12 @@
 var BasicGame = {};
 
-BasicGame.Boot = function (game) {
+BasicGame.Boot = function(game) {
 
 };
 
 BasicGame.Boot.prototype = {
 
-    preload: function () {
+    preload: function() {
 
         //  Here we load the assets required for our preloader (in this case a background and a loading bar)
         this.load.image('preloaderBackground', 'assets/bg.png');
@@ -14,7 +14,7 @@ BasicGame.Boot.prototype = {
 
     },
 
-    create: function () {
+    create: function() {
 
         //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
         this.input.maxPointers = 1;
@@ -22,13 +22,10 @@ BasicGame.Boot.prototype = {
         //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
         this.stage.disableVisibilityChange = true;
 
-        if (this.game.device.desktop)
-        {
+        if (this.game.device.desktop) {
             //  If you have any desktop specific settings, they can go in here
             this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
+        } else {
             //  Same goes for mobile settings.
             //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
