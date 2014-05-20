8754edaeb2704066921d4ceb057562ed30713d3a
André Tavares <atb.tavares@gmail.com>
2014-05-20 07:44:34 -0400

    exemplos da aula de sabado e testes

--- halison/andretavares/dinossauro/js/game.js
+++ halison/andretavares/dinossauro/js/game.js
@@ -3,6 +3,7 @@ var dinoSprite, ossos;
 
 var game;
 
+var velocidade = 200;
 game = new Phaser.Game(800, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload () {
@@ -19,12 +20,14 @@ function create () {
 	dinoSprite = game.add.sprite(400, 400, 'dino');
 	dinoSprite.animations.add('walk',[1,2,3,4],15,true); // ( <string de identificacao>, <numero do sprite>, <velocidade da animcacao>, <true?>) 
 	dinoSprite.animations.add('jump',[4,5,6],4,true);
+	dinoSprite.animations.add('diagjump',[4,5,6,5,3,2],4,true);
+
 	game.physics.enable(dinoSprite, Phaser.Physics.ARCADE); // permite que a sprite tenha um corpo fisico
     dinoSprite.body.acceleration.y = 50;
     dinoSprite.body.collideWorldBounds = true; // para no limite inferior da tela
     dinoSprite.body.drag.x = 100; //desloca 100 e para, sÃ³ desloca de novo se clicada alguma tecla e quanto maior for seu valor, menos desloca
     dinoSprite.anchor.setTo(.5,.5); // diminui o espaço do deslocamento do espelhamento 
-    dinoSprite.body.gravity.y = 250;
+    dinoSprite.body.gravity.y =1000;
    
     // Criar o osso
     ossos = game.add.group();
@@ -40,30 +43,33 @@ function update () {
 	// Realizar colisão entre 'osso' e 'dino', chama função dinoEatOsso
 	game.physics.arcade.overlap(dinoSprite, ossos, dinoEatOsso,null,this);
 
+	// Capturar entrada (tecla cimea)
+//	 if ( game.input.keyboard.isDown (Phaser.Keyboard.UP &&Phaser.Keyboard.LEFT ) ) { // vai para cima
+//	}  
 	// Capturar entrada (tecla esquerda)	
+
 	if ( game.input.keyboard.isDown (Phaser.Keyboard.LEFT) ) { 
-		dinoSprite.body.velocity.x = -100;
+		dinoSprite.body.velocity.x = -velocidade;
 		dinoSprite.animations.play('walk');
-		dinoSprite.scale.x = -1; // espelha se antes -1
+		dinoSprite.scale.x = -1; // muda a direção, espelhando a imagem
 	}
 
 	// Capturar entrada (tecla direita)
 	else if ( game.input.keyboard.isDown (Phaser.Keyboard.RIGHT) ) {
-		dinoSprite.body.velocity.x = 100;
+		dinoSprite.body.velocity.x = velocidade;
 		dinoSprite.scale.x = 1;
 		dinoSprite.animations.play('walk');
 	}
 
+	// Capturar entrada (tecla cimea)
 	else if ( game.input.keyboard.isDown (Phaser.Keyboard.UP) ) { // vai para cima
-
-		dinoSprite.body.velocity.y = -100;
+		dinoSprite.body.velocity.y = -velocidade;
 		dinoSprite.animations.play('jump');
-	}
-
-	else{
+		
+	} else {
 	    	dinoSprite.animations.stop();
 			dinoSprite.frame = 0;
-		}	
+	}	
 }
 
 function dinoEatOsso (dino,osso)	{
