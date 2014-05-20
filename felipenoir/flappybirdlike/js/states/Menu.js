9778b0c78866f31b622cce7e6f8e2e7a7873f2c4
unknown <marcos.castro@marcos-castro.dom.fpf.br>
2014-05-20 13:47:25 -0400

    menu e audio do menu

--- felipenoir/flappybirdlike/js/states/Menu.js
+++ felipenoir/flappybirdlike/js/states/Menu.js
@@ -1,10 +1,32 @@
+
+var audioMenu;
 //Menu.js
-var menuState = {  
+var menuState = { 
+	
+	
+	
+	preload: function(){
+		game.load.image('logoMenu', 'assets/phaser2.png');
+		game.load.audio('audioMenu', 'assets/song_menu_otimizada.mp3');
+
+	},
+ 
     create: function() {
-        this.start();
+		var spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		spaceBar.onDown.add(this.start,this);
+		
+		//Toca audio do menu
+		audioMenu = game.add.audio('audioMenu',1,true);
+		audioMenu.play('',0,1,true);
+		
+		//Animacao do menu
+		var spriteLogoMenu =  game.add.sprite(this.game.world.width/2, -50, 'logoMenu', 0);
+		spriteLogoMenu.anchor.setTo(0.5, 0.5);
+		game.add.tween(spriteLogoMenu).to( { y: this.game.world.height/2 }, 2400, Phaser.Easing.Bounce.Out, true);
     },
 	
     start: function() {
+		audioMenu.stop();
         this.game.state.start('play');
     }
 };
