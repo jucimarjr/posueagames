4ac89fdef5eed6bb9481d770a98e83d85022ee68
JulianaFigueira <juli.figueira@gmail.com>
2014-05-21 00:49:57 -0400

    Testando unir nova estrutura de arquivos, paralax e base para o jogo

--- rafaelima/FlappyBird/js/game.js
+++ rafaelima/FlappyBird/js/game.js
@@ -3,6 +3,16 @@ var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game_div');
 //variavel global
 var score = 0;
 
+var billySprite;
+var plataformas;
+var space_key;
+
+//paralax
+var background1;
+var background2;
+var background3;
+var background4;
+
 // Define all the states
 game.state.add('load', load_state);  
 game.state.add('menu', menu_state);  
