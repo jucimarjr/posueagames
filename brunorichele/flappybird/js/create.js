9af400362f6e001becbbbab65c201b3d091acb31
crisoara <crisoara@gmail.com>
2014-05-22 01:37:05 -0400

    ajustes do segundo plano de fundo

--- brunorichele/flappybird/js/create.js
+++ brunorichele/flappybird/js/create.js
@@ -18,11 +18,11 @@ var create = {
         // TODO: parallax background     
         //Tentativa de parallax background
         
-    	tilesprite = game.add.tileSprite(0, 0, 960, 600, 'background');
-    	tilesprite.autoScroll(-100, 0);
+    	tilesprite = game.add.tileSprite(0, 0, 960,600,  'background');
+    	tilesprite.autoScroll(-200, 0);
 
-    	//tilespritedois = game.add.tileSprite(0, game.world.height - 100, 960, 100, 'river');
-    	//tilespritedois.autoScroll(-100, 0);
+    	tilespritedois = game.add.tileSprite(0, 500, 960, 100, 'river');
+    	tilespritedois.autoScroll(-100, 0);
         
 
         //player defs
