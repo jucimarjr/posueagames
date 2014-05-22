71922e309c2abe7cb28c1e4e6a86b14bd3888159
Rodrigo de Souza Braga <rodrigogrow@gmail.com>
2014-05-21 20:10:15 -0400

    add images on menu view

--- rageloop/flappyJedi/js/states/preload.js
+++ rageloop/flappyJedi/js/states/preload.js
@@ -7,18 +7,19 @@
             //this.stage.backgroundColor = '#ccc';
 
             /*menu images*/
-            this.game.load.image('playbtn','assets/menu/playbtn_160-160.jpg');            
+            this.game.load.image('menubg','assets/menu/bgmenu_960-600.png');
+            this.game.load.image('playbtn','assets/menu/btnplay1_50-50.png');
+
+            /*gameover images*/
             this.game.load.image('replaybtn','assets/menu/play_again_button.png');
             this.game.load.image('gameover','assets/menu/gameover.png');
             
             /*gameplay images*/
-
             this.game.load.image('player', './assets/player_205-94.png');
             this.game.load.image('bg', './assets/bg_1600-600.jpg');
             this.game.load.image('fg', './assets/fg_1998-224.png');
             this.game.load.image('enemy', './assets/enemy_205-94.png');
-            this.game.load.image('bullet', './assets/bullet_43-43.png');
-            
+            this.game.load.image('bullet', './assets/bullet_43-43.png');            
             this.game.load.spritesheet('powerups', './assets/powerups_240-60.png', 60, 60);
 
         },
