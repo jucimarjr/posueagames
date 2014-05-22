8f9ebb0e7e1c0f5d076e383616752597046a6c8a
crisoara <crisoara@gmail.com>
2014-05-22 00:59:18 -0400

    ajustando imagem de fundo

--- brunorichele/flappybird/js/create.js
+++ brunorichele/flappybird/js/create.js
@@ -11,13 +11,15 @@ var create = {
         //game defs
         game.world.setBounds(0, 0, 960, 800);
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        game.add.sprite(0,0,'background');
 
         // background
         // TODO: parallax background     
         //Tentativa de parallax background
         
-    	tilesprite = game.add.tileSprite(0, 0, 960, 800, 'background');
-    	tilesprite.autoScroll(-200, 0);
+    	//tilesprite = game.add.tileSprite(0, 0, 960, 800, 'background');
+    	//tilesprite.autoScroll(-200, 0);
 
     	//tilespritedois = game.add.tileSprite(0, game.world.height - 100, 960, 100, 'river');
     	//tilespritedois.autoScroll(-100, 0);
