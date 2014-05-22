bba90566f641b2dcd502e0e74cefba203be9a6b3
Andre Luiz <ziulerdna71@yahoo.com>
2014-05-22 10:25:03 -0400

    Ajuste paralax, troca ceu, montanhas

--- carlosborges2/flapdragon/js/Level.js
+++ carlosborges2/flapdragon/js/Level.js
@@ -1,12 +1,14 @@
 
-var BG_SKY = PATH_ASSETS + 'sky_960-600.png';
-var BG_GROUND = PATH_ASSETS + 'mountain_1366-500.png';
-
+var BG_SKY = PATH_ASSETS + 'skyday_960-600.png';
+//var BG_GROUND = PATH_ASSETS + 'mountain_1366-500.png';
+var BG_GROUND = PATH_ASSETS + 'mountains_1280-150.png';
+var BG_BIGCITY = PATH_ASSETS + 'bg_city_1000-350.png';
 Level = function(/*game*/) {
 	
 //	this.game = game;
 	this.ground;
 	this.bgSky;
+	this.bigcity;
 
 	this.preload = function() {
 		
@@ -14,7 +16,7 @@ Level = function(/*game*/) {
 		
 		game.load.image(BG_SKY, BG_SKY);
 		game.load.image(BG_GROUND, BG_GROUND);
-		
+		game.load.image(BG_BIGCITY, BG_BIGCITY);
 	};
 
 	this.create = function() {
@@ -23,7 +25,11 @@ Level = function(/*game*/) {
 		
 		// ground
 		this.ground = game.add.tileSprite(0, game.world.height - 90, game.world.width, 90, BG_GROUND);
-		this.ground.autoScroll(-200, 0);
+		this.ground.autoScroll(-10, 0);
+		
+		//bigcity
+		this.bigcity = game.add.tileSprite(0, game.world.height - 90, game.world.width, 90, BG_BIGCITY);
+		this.bigcity.autoScroll(-50, 0);
 		
 		// physics
 		game.physics.enable(this.ground, Phaser.Physics.ARCADE);
