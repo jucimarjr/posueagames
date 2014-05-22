5dfee1c0139a73c2cf3b241c30d473852814ad26
maquineh <rapiner@gmail.com>
2014-05-22 02:44:21 -0300

    Adicionado a pontuação e colisão com o topo e com o chão

--- yasminetkn/hellcopter/js/game.js
+++ yasminetkn/hellcopter/js/game.js
@@ -32,7 +32,9 @@ function create () {
 
 	game.add.sprite(0, 0, 'chao');
 	game.add.sprite(0, 0, 'top');
-
+	
+	
+	
     //faz a chao rolar em loop
     this.ground2 = this.game.add.tileSprite(0, 0, 960, 600, 'ground');//(x,y,tamanho em x,?)
     this.ground2.autoScroll(-20, 0);
@@ -51,46 +53,63 @@ function create () {
     
     this.top.body.immovable = true;
     this.top.body.width = this.game.world.width;
-
+    
+    game.input.keyboard.addKeyCapture([Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR]);
+    cursors = game.input.keyboard.createCursorKeys();
 
     music.play('sobe');
 	
 	// Criando o Helicoptero
 	bonecoSprite = game.add.sprite(128, 80, 'helicoptero');
-	bonecoSprite.animations.add('walk',[1,4],4,true);
+	bonecoSprite.animations.add('jumpHeli',[0,3],4,true).play();
 
 	game.physics.enable(bonecoSprite, Phaser.Physics.ARCADE); // permite que a sprite tenha um corpo fisico
 
-    bonecoSprite.body.velocity.y=-350;
+    bonecoSprite.body.velocity.y=-250;
     bonecoSprite.body.gravity.y=1000;
 
     bonecoSprite.body.collideWorldBounds = true; // para no limite inferio da tela
-//    bonecoSprite.anchor.setTo(.5,.5); // diminui o espaco do deslocamento do espelhamento
+
     game.add.tween(this.player).to({angle: -15}, 300);
 
     ledges = game.add.group();
     ledges.enableBody = true;
-   /* obstacles = game.add.group();
-    obstacles.enableBody = true;
-    obstacles.createMultiple(20,'bloco');*/
-    game.time.events.loop(900, createBlocos, this);
-    //this.timer = this.
-    //game.time.events.loop(1500, addLinhaDeObstaculos(), this);
 
+    game.time.events.loop(950, createBlocos, this);
+    
+    game.input.onDown.add(listener, this);
+    
+    text = game.add.text(game.world.width - 150, 15, "Pontuação: 0", {
+    	font: "25px Arial",
+    	fill: "#fff",
+    	align: "center"
+    }); 
+    
+    text2 = game.add.text(game.world.centerX, game.world.centerY, "", {
+    	font: "25px Arial",
+    	fill: "#fff",
+    	align: "center"
+    });
+    text.anchor.setTo(0.5, 0.5);
 
+    text4 = game.add.text(game.world.width - 150, 45, "Highscore: 0", {
+    	font: "25px Arial",
+    	fill: "#fff",
+    	align: "center"
+    });
+    	text4.anchor.setTo(0.5, 0.5); 
 }
 
 function update () {
-    if ( game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR) ) { // tecla do pulo
-        bonecoSprite.body.velocity.y = -350;
-        //player.body.gravity.y = 650;
-        //game.time.events.loop(1500, createBlocos, this);
-        start = 1;
-        count = 0; 
-    }
-    game.physics.arcade.collide(bonecoSprite, this.ground);
-    game.physics.arcade.collide(bonecoSprite, this.top);
-    game.physics.arcade.collide(bonecoSprite, ledges);
+	
+/*	if(start == 0){*/
+		if ( game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR) ) { // tecla do pulo
+	        bonecoSprite.body.velocity.y = -250;
+	    }
+//	}
+	
+	//Verifica se chocou com o topo, o chão ou com blocos	
+    game.physics.arcade.collide(bonecoSprite, [this.ground, this.top, ledges], gameEnd, null, this);
     game.physics.arcade.overlap(bonecoSprite, ledges, gameEnd, null, this); 
 
 }
@@ -98,32 +117,45 @@ function update () {
 
 function createBlocos() {
 	ledge = ledges.create(game.world.width, game.rnd.integerInRange(35, game.world.height) - 100, 'bloco');
-	
-	
-	
 	ledge.body.velocity.x = -950;
 	ledge.body.immovable = true;
 	ledge.outOfBoundsKill = true;
+	
+	count = count + 25;
+	text.setText("Pontuação: " + count);
 } 
 
 function gameEnd(player, ledge) {
 	bonecoSprite.kill();
 	//explosion.play();
 	game.time.events.events = [];
-	ledge.kill();
-	//text2.setText("Game over. Your points: " + count + "\nPress SPACE to restart.");
-	//text2.anchor.setTo(0.5, 0.5);
-	bonecoSprite.x = 150;
+	
+	if(ledge==null){
+		//ledge.kill();
+	}
+
+	text2.setText("Game over. Pontuação: " + count + "\nPressione SPACE para reiniciar.");
+	text2.anchor.setTo(0.5, 0.5);
+	
+	/*bonecoSprite.x = 150;
 	bonecoSprite.y = game.world.centerY - 55;
 	bonecoSprite.body.velocity.y = 0;
 	bonecoSprite.body.velocity.x = 0;
 	bonecoSprite.body.gravity.y = 0;
-	start = 0;
-	/*if (highscore < count) {
-	highscore = count;
-	text4.setText("Your highscore: " + highscore);
-	}*/
+	start = 0;*/
+	
+	if (highscore < count) {
+		highscore = count;
+		text4.setText("Seu maior placar eh: " + highscore);
+	}
 } 
+
+function listener() {
+	
+	if (start == 1) {
+		player.body.velocity.y = -250;
+	}
+}
 
 
 /*function addUmObstacle(x,y){
