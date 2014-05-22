0c9b5d07fd6ed65759fe792c91b58c4a30ca144c
Sirineo Filho <ext-sirineo.filho@microsoft.com>
2014-05-22 03:35:51 -0400

    Criação e movimentação dos asteroides

--- saidesigner/supermouse/js/Game.js
+++ saidesigner/supermouse/js/Game.js
@@ -1,22 +1,77 @@
 
 var SuperMouse  = {};
 
-
 SuperMouse.Game = function(game) {
 	
-	this.game;
-
+	this.asteroids;
+	this.sameColumn = false;	
 }; 
 
 
 SuperMouse.Game.prototype.preload = function() {
-	alert('preload');
+	
+	this.load.image('universe', 'assets/universe_900-600.png');
+    this.load.image('asteroid', 'assets/asteroid_80-80.png');
+
 };
 
 SuperMouse.Game.prototype.create = function() {
-	alert('create');
+
+	this.add.sprite(0, 0, 'universe');
+	
+	this.asteroids = this.add.group();
+	this.asteroids.enableBody = true;
+
+	var lastPosX = 600;
+	var lastPosY = 0;	
+	var i = 0;
+
+	do {
+		if (this.sameColumn) {			
+			lastPosY = lastPosY + 300 < 600 ? lastPosY + 300 : lastPosY - 300;
+			this.sameColumn = false;
+	    } else {
+	    	lastPosX += 400;
+	    	lastPosY = Math.floor((Math.random() * 500) + 1);
+	    	this.sameColumn = (Math.floor((Math.random() * 100) + 1) % 2) == 0;
+	    }
+
+	    asteroid = this.asteroids.create(lastPosX, lastPosY, 'asteroid');
+	    asteroid.body.immovable  = true;    
+	    asteroid.body.allowRotation = true;
+	    asteroid.body.velocity.x = -150;
+	    asteroid.body.angularVelocity = Math.floor((Math.random() * 10) + 5) * -1;
+
+	} while (++i < 6);
+
+	this.asteroids.sort('x', Phaser.Group.SORT_ASCENDING);
 };
 
 SuperMouse.Game.prototype.update = function() {
-	alert('update');
+
+	var lastAsteroid = this.asteroids.getAt(5);
+
+	var lastPosX = lastAsteroid.body.x;
+	var lastPosY = lastAsteroid.body.y;
+	
+	for (var i = 0; i < 6; i++) {
+
+		var asteroid = this.asteroids.getAt(i);
+
+		if (asteroid.body.x < 0) {
+			if (this.sameColumn) {			
+				lastPosY = lastPosY + 300 < 600 ? lastPosY + 300 : lastPosY - 300;
+				this.sameColumn = false;
+		    } else {
+		    	lastPosX += 400;
+		    	lastPosY = Math.floor((Math.random() * 500) + 1);
+		    	this.sameColumn = (Math.floor((Math.random() * 100) + 1) % 2) == 0;
+		    }
+
+		    asteroid.body.x = lastPosX;
+		    asteroid.body.y = lastPosY;
+		}
+	}
+
+	this.asteroids.sort('x', Phaser.Group.SORT_ASCENDING);
 };
