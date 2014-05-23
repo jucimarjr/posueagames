cb88f811a013fce8644240ed57e03478738ac736
Andrediasdelima <andredias_m@hotmail.com>
2014-05-23 02:05:26 -0400

    adicionando assets

--- renanbrg/dr_who/js/game.js
+++ renanbrg/dr_who/js/game.js
@@ -7,7 +7,14 @@ var snd_inicio, snd_musica;
 
 function preload () {
     game.load.image('background', 'assets/background_960-600.png');
-    game.load.image('meteoro', 'assets/meteor_170-135.png');
+    game.load.image('meteoro', 'assets/meteoro_120-135.png');
+    game.load.image('meteoro2', 'assets/meteoro2_120-135.png');
+    game.load.image('meteoro3', 'assets/meteoro3_120-135.png');
+    game.load.image('meteoro4', 'assets/meteoro4_120-135.png');
+    game.load.image('meteoro5', 'assets/meteoro5_120-135.png');
+    game.load.image('meteoro6', 'assets/meteoro6_120-135.png');
+    game.load.image('meteoro7', 'assets/meteoro7_120-135.png');
+    game.load.image('meteoro8', 'assets/meteoro8_120-135.png');
     game.load.image('tardis', 'assets/tardis_77-110.png');
     snd_inicial = document.getElementById("inicial");
     snd_musica = document.getElementById("musica");
