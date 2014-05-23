349cb85dae6e456c385cbb874e201f0d36080e4a
Helder Batista <hcb007@gmail.com>
2014-05-23 14:05:14 -0400

    Estruturando o jogo em varios arquivos

--- lenasottomayor/FlapBird_5-45/js/load.js
+++ lenasottomayor/FlapBird_5-45/js/load.js
@@ -1,14 +1,14 @@
 var loadGame = { preload: preload, create: create };
 
 function preload() {
-	game.load.image('startMenu', 'assets/5_45_start_screen_960-600.jpg');
-	game.load.image('finalCredits', 'assets/5_45_start_screen_960-600.jpg');
-	game.load.image('score', 'assets/5_45_game_score_960-600.jpg');
+	game.load.image('startMenu', 'assets/startscreen_960-600.png');
+	game.load.image('creditscreen', 'assets/creditscreen_960-600.png');
+	game.load.image('score', 'assets/scorescreen_960-600.png');
 	game.load.image('obstacle', 'assets/Obstacle_129-509.png');
 	game.load.spritesheet('player', 'assets/manspritesshet_485-131-4.png', 121.25,131);
-	game.load.image('sky', 'assets/sky_1920-600.png');
+	game.load.image('background', 'assets/background_980-600.png');
 }
 
 function create() {
-    game.state.start(MENU);
+    game.state.start('menu');
 }
