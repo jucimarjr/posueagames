0b24cc1cbaaa02b489c74cbe6bb8c273707c6cdf
diegonobu <diegonobu@gmail.com>
2014-05-24 02:13:31 -0400

    alterando a tela de aviso

--- felipenoir/flappybirdlike/js/game.js
+++ felipenoir/flappybirdlike/js/game.js
@@ -41,5 +41,5 @@ game.state.add('menu', menuState);
 game.state.add('play', playState);
 game.state.add('gameover', gameoverState);
 
-// game.state.start('warning');
-game.state.start('load');
+game.state.start('warning');
+// game.state.start('load');
