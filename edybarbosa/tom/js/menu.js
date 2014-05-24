d7387f059cab8e3cf896b0890a544e6736932774
Railton Nepomuceno <railton@ryolabs.com>
2014-05-23 23:01:54 -0400

    Integrando funcionalidades

---
+++ edybarbosa/tom/js/menu.js
@@ -0,0 +1,26 @@
+Game.Menu = function (game) { };
+
+Game.Menu.prototype = {
+
+	create: function () {
+
+	   	game.add.sprite(0, 0, 'abertura');
+
+	    curto = game.add.text(850, 50, '(a) Pulo curto ', { font: '15px Nunito', fill: '#FFFFFF' });
+	    longo = game.add.text(850, 70, '(s) Pulo longo', { font: '15px Nunito', fill: '#FFFFFF' });
+
+		iniciar = game.add.sprite(30, h-200,'iniciar');
+		iniciar.animations.add('run');
+		iniciar.animations.play('run', 10, true);
+
+		i = game.input.keyboard.addKey(Phaser.Keyboard.I);
+	},
+	update: function () {
+		
+		if (i.isDown) {
+			game.state.start('Play');
+	    }
+			
+	},	
+
+};
