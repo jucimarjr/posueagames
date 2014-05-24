f985e44caba5287d546fcf76a322ddb9a2be296f
HelderBatista <hcb007@gmail.com>
2014-05-24 14:59:47 -0400

    Adjusting jump, colision and score

--- lenasottomayor/FlapBird_5-45/js/play.js
+++ lenasottomayor/FlapBird_5-45/js/play.js
@@ -3,11 +3,11 @@ var playGame = { create: create, update: update };
 function create() {
 	
 	background = game.add.tileSprite(0, 0, game.stage.bounds.width,game.cache.getImage('background').height, 'background');
-	background.autoScroll(-100, 0);	 
+	background.autoScroll(-60, 0);	 
 	
 	computer = game.add.sprite(0, 264,'computer');
 	game.physics.enable(computer, Phaser.Physics.ARCADE);
-	computer.body.velocity.x = -100;
+	computer.body.velocity.x = -60;
 	
 	
 	plataforms = game.add.group();
@@ -15,13 +15,15 @@ function create() {
 	
 	plataform = plataforms.create(0, 440, 'plataform');
 	game.physics.enable(plataform, Phaser.Physics.ARCADE);
-	plataform.body.velocity.x = -100;
+	plataform.body.velocity.x = -60;
 	plataform.body.immovable = true;
 	
 	createObstacles();
 	createPlayer();
 	
-	
+	score = 0;
+	jumps = 0;
+	collide = false;
 
 	musicMenu.stop();
 	
@@ -46,7 +48,7 @@ function update() {
 }
 
 function createPlayer() {
-	playerSprite = game.add.sprite(150, 309, 'player');
+	playerSprite = game.add.sprite(200, 309, 'player');
 	playerSprite.animations.add('jump',[2],7,true);
 	playerSprite.animations.add('dead',[3,2,3,2],4,true)
 	playerSprite.frame = 1;
@@ -58,16 +60,19 @@ function createPlayer() {
 	playerSprite.body.gravity.y = 200;
 	playerSprite.anchor.setTo(0.5,0.5);
 	playerSprite.body.acceleration.y = 500;
-//	playerSprite.body.collideWorldBounds = true;
 }
 
 function jump() {
-	playerSprite.body.velocity.y = -400;
-	playerSprite.body.velocity.x = 2000;
-	playerSprite.animations.play('jump');
-	this.jumpSound = game.add.audio('jumpSound');
-    this.jumpSound.play();
-
+	
+	if(jumps<2) {
+		jumps++;
+		collide = false;
+		playerSprite.body.velocity.y = -500;
+		playerSprite.body.velocity.x = 400;
+		playerSprite.animations.play('jump');
+		this.jumpSound = game.add.audio('jumpSound');
+	    this.jumpSound.play();
+	}
 }
 
 function stop() {
@@ -77,57 +82,51 @@ function stop() {
 
 function createObstacles() {
 	obstacles = game.add.group();
+	obstacles.createMultiple(20,'obstacle');
 	obstacles.enableBody = true;
-    this.timer = this.game.time.events.loop(1700, addObstacle, this);
+    this.timer = this.game.time.events.loop(1500, addObstacle, this);
 }
 
 function addObstacle() {
 	var heigth = game.world.randomY;
 
 	if(heigth > 250 && heigth < 400) {
-		obstacle = obstacles.create(960, heigth+100, 'obstacle');
+		obstacle = obstacles.getFirstDead();
+		obstacle.reset(960, heigth+100);
 		game.physics.enable(obstacle, Phaser.Physics.ARCADE);
-		obstacle.body.velocity.x = -200;
+		obstacle.body.velocity.x = -250;
 		obstacle.body.immovable = true;
+		obstacle.outOfBoundsKill = true;
 	} else if (heigth <= 250) {
-		obstacle = obstacles.create(960, heigth+250, 'obstacle');
+		obstacle = obstacles.getFirstDead();
+		obstacle.reset(960, heigth+250);
 		game.physics.enable(obstacle, Phaser.Physics.ARCADE);
-		obstacle.body.velocity.x = -200;
+		obstacle.body.velocity.x = -250;
 		obstacle.body.immovable = true;
+		obstacle.outOfBoundsKill = true;
 	} else {
-		obstacle = obstacles.create(960, heigth-150, 'obstacle');
+		obstacle = obstacles.getFirstDead();
+		obstacle.reset(960, heigth-150);
 		game.physics.enable(obstacle, Phaser.Physics.ARCADE);
-		obstacle.body.velocity.x = -200;
+		obstacle.body.velocity.x = -250;
 		obstacle.body.immovable = true;
+		obstacle.outOfBoundsKill = true;
 	}
 }
 
-function gameOver(){
-	
-	game.physics.arcade.collide(playerSprite, obstacles);
-	if((playerSprite.position.y > obstacle.position.y)){
+function gameOver(playerSprite,obstacle){
+	if((playerSprite.position.y < obstacle.position.y)){
+		game.physics.arcade.collide(playerSprite, obstacles);
+		jumps=0;
+		if(collide == false){
+			score++;
+			collide = true;
+		}
+	} else {	
 		playerSprite.frame = 3;
 		musicGame.stop();
 		deadSound = game.add.audio('deadSound');
 		deadSound.play('',0,0.2,false);
 		game.state.start('gameScore');
 	}
-	
-	
-//	game.physics.arcade.collide(playerSprite, obstacles);
-//	if((playerSprite.position.y > obstacle.position.y)){
-//		if ((playerSprite.position.x < obstacle.position.x) && (playerSprite.position.x  > obstacle.position.x+129)) {
-//
-//
-//			playerSprite.frame = 3;
-//		} else {
-//			musicGame.stop();
-//			game.state.start('gameScore');
-//		}
-//	}
-
-	
-//	playerSprite.frame = 3;
-//	musicGame.stop();
-//	game.state.start('gameScore');
 }
