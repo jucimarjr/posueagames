036bbdebe9b1189a3f9319e52b95518292c529f8
maquineh <rapiner@gmail.com>
2014-05-24 05:45:49 -0300

    adicionado background, menu som do menu, nova estrutura de JS para states e ajustes na mecânica do jogo

---
+++ yasminetkn/hellcopter/js/menu.js
@@ -0,0 +1,47 @@
+var timer2 =0,  text5;
+
+var styleText2 = {
+		font : "25px Arial",
+		fill : "#fff"
+};
+
+var gameMenuHell = {
+		
+		preload: function(){
+			  game.load.image('initBg', 'assets/background.png');
+		      game.load.audio('audio', 'assets/menu.ogg');
+		},
+		
+		create: function(){
+			audio = game.add.audio('audio');
+			audio.addMarker ('audioM',0,30,2,true);
+			audio.play('audioM', 0, 1, true);
+			var initBg = game.add.tileSprite(25, 0, 910, 600, 'initBg');
+		    text5 = game.add.text(200, game.world.centerY, "Press Space Key To Start", styleText2);
+		    
+		    
+		    var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		    spaceKey.onDown.add(this.start, this);
+		    
+		    /*if ( game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR) ) {
+				this.start();
+			 }*/
+		}, 
+		
+		update: function(){
+			 	timer2 += game.time.elapsed; 
+			    if ( timer2 >= 800 )
+			    {
+			        timer2 -= 800;
+			        text5.visible = !text5.visible;
+			    }
+		},
+		
+		start: function(){
+			 console.log("menu start");
+			 audio.stop('audioM');
+			 game.state.start('fase');
+		}
+		
+		
+};
