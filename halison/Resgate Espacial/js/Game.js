8d6825be5604c7d7b4801a9a01b3ab7cb8505936
André Tavares <atb.tavares@gmail.com>
2014-05-23 21:54:57 -0400

    incluida animacao dos asteroids, variando scale e velocidade

--- halison/Resgate Espacial/js/Game.js
+++ halison/Resgate Espacial/js/Game.js
@@ -1,55 +1,64 @@
-var naveSprites, asteroid, fundo;
 
-var game = new Phaser.Game(960, 500, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(960, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload () {
-
-	game.stage.backgroundColor = "#000000";
-	game.load.spritesheet('nave', 'assets/nave.png', 200,160); // 200x160 eh o tamanho do frame da sprite
-	game.load.image('vol1', 'assets/vol1.png');
-	game.load.image('fundo', 'assets/fundo_960-600');
-    game.load.image('asteroid', 'assets/asteroid_80-80.png');
+	game.load.image('nave', 'assets/nave_100-40.png');
+	game.load.image('space', 'assets/universo.png');
+	game.load.image('asteroid', 'assets/asteroid_80-80.png');
 }
+
+var asteroid;
+var space;
+var nave;
+var group;
 
 function create () {
-	//fundo
 	
-	var bg = game.add.tileSprite(0, 0, game.stage.bounds.width,game.cache.getImage('fundo').height, 'fundo');
-    bg.autoScroll(-200, 0);
+    this.space = this.game.add.tileSprite(0, 0, 960, 600, 'space');
+    this.space.autoScroll(-150, 0);
 	
-	// CREATE A nave
-	naveSprite = game.add.sprite(200, 0, 'nave');
-	naveSprite.animations.add('walk',[1,2],6,true);
-	naveSprite.animations.add('jump',[3,4,5],4,true);
-	game.physics.enable(dinoSprite, Phaser.Physics.ARCADE); // permite que a sprite tenha um corpo fisico
-   
-    naveSprite.body.acceleration.y = 100;
- 
-    naveSprite.body.collideWorldBounds = true; // para no limite inferio da tela
-    naveSprite.body.drag.x = 100; //desloca 100 e para, só desloca de novo se clicada alguma tecla e quanto maior for seu valor, menos desloca
-    naveSprite.anchor.setTo(.5,.5); // diminui o espaco do deslocamento do espelhamento 
-    naveSprite.body.gravity.y = 150;
-    
-   
-    // CREATE A Voluntario:
-    vol1 = game.add.group();
-    vol1.create( 500, 50, 'vol1');
-    game.physics.enable(vol1, Phaser.Physics.ARCADE);
-    
-    // cria o grupo para plataformas
-    fundo = game.add.group();
-    fundo.enableBody = true;
-    
-    //cria um bloco para o dino ficar em cima
-    var asteroid = asteroid.create(350, 250, 'asteroid');
-    bloco.body.immovable = true; // deixa o bloco imovivel
-    
-    // cria  c
-    //var tablado = plataformas.create(0, 450, 'bloco');
-    //tablado.body.immovable = true;
-    //tablado.scale.setTo(2, 2); // amplia o bloco pra ficar o chao todo
+	nave = game.add.sprite(0, 0, 'nave');
+	game.physics.enable(nave, Phaser.Physics.ARCADE);
+	nave.body.velocity.y = 10;
+	   
+//	asteroid = game.add.sprite(game.world.width,100, 'asteroid');
+//	game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+//	asteroid.body.velocity.x = -150;
+//	asteroid.anchor.set(0.5);
+//	asteroid.smoothed = false;
+//	game.add.tween(asteroid.scale).to( { x: 2.05, y: 2.05 }, 1400, Phaser.Easing.Linear.None, true, 0, 1000, true);
+	
+	group = game.add.group();
+	for (var i = 0; i < 4; i++)
+    {
+    	sprite = group.create(500+(200 *i), game.rnd.integerInRange(1, 600), 'asteroid', 'asteroid0000');
+    	game.physics.enable(sprite, Phaser.Physics.ARCADE); 
+    	game.add.tween(sprite.scale).to( { x: 1.4, y: 1.4 }, game.rnd.integerInRange(700,1300), Phaser.Easing.Linear.None, true, 0, 1000, true);
+    	sprite.body.velocity.x = game.rnd.integerInRange(-150,-50);
+    }
+	var frameNames = Phaser.Animation.generateFrameNames('asteoid', 0, 24, '', 4);
+	
 
-
+//	child.animations.add('swim', frameNames, 30, true, false)
+	 
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
 }
 
+function update() {
+	
+//	if (asteroid.x < -asteroid.width){
+//		asteroid.x = game.world.width;
+//    }
 
+    if (upKey.isDown)
+    {
+    	nave.y-= 2;
+    }
+    else if (downKey.isDown)
+    {
+    	nave.y+= 2;
+    }
+//	game.physics.arcade.collide(nave, asteroid);
+
+}
