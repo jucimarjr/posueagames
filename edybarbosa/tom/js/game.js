0a0d5d48177dca27130d2b04509cf4b01b7c94eb
rodrigodiniz <rodrigodiniz1@gmail.com>
2014-05-27 15:08:07 -0400

    add fb button

--- edybarbosa/tom/js/game.js
+++ edybarbosa/tom/js/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(w, h, Phaser.AUTO, 'gameContainer');
+var game = new Phaser.Game(w, h, Phaser.AUTO, 'game');
 
 game.state.add('Boot', Game.Boot);
 game.state.add('Load', Game.Load);
