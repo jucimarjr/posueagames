b3c8da161a1fb2f08c54c633111a4b350bc99015
Andrediasdelima <andredias_m@hotmail.com>
2014-05-28 00:11:20 -0400

    sprite sheet atualization

--- renanbrg/dr_who/js/preload.js
+++ renanbrg/dr_who/js/preload.js
@@ -12,7 +12,7 @@ function preload() {
     game.load.image('meteor7', 'assets/meteoro7_135-120.png');
     game.load.image('meteor8', 'assets/meteoro8_135-120.png');
     game.load.spritesheet('tardis', 'assets/tardissheet_600-110.png', 75, 110);
-    game.load.spritesheet('telaInicial', 'assets/spriteSheet_5760-600.png', 5760, 600);
+    game.load.spritesheet('telaInicial', 'assets/spriteSheetTelaInicial_7680-600.png', 7680, 600);
     game.load.spritesheet('telaInicial1', 'assets/spriteSheetTelaInicial1_1920-600.png', 960, 600);
     game.load.image('telaInicio', 'assets/tela_incial1_960-600.png');
     game.load.image('telaInicio2', 'assets/tela_incial2_960-600.png');
