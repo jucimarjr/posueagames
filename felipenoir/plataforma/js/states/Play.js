d2285dd35eb6327f58c6f848b539b37dfab96af5
diegonobu <diegonobu@gmail.com>
2014-05-29 09:30:38 -0400

    primeira fase em construção

--- felipenoir/plataforma/js/states/Play.js
+++ felipenoir/plataforma/js/states/Play.js
@@ -1,9 +1,12 @@
 State.Play = function() {
+    var map, layer, hero;
 };
 
 State.Play.prototype = {
     preload:function() {
-        game.load.image('bg','assets/images/bg1.jpg');
+        game.load.tilemap('map', Level.tilemap.path, null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tileset', Level.tileset.path);
+        game.load.image('bg', Level.bg.path);
         game.load.image('hero', Hero.path);
     },
 
@@ -11,12 +14,24 @@ State.Play.prototype = {
         game.physics.startSystem(Phaser.Game.ARCADE);
         game.physics.arcade.gravity.y=800;
 
-        game.add.sprite(0,0,'bg');
+        bg = game.add.sprite(0,0,'bg');
+        bg.fixedToCamera = true;
 
-        var hero = game.add.sprite(0,0,'hero');
+        map = game.add.tilemap('map');
+        map.addTilesetImage('tileset','tileset');
+
+        layer = map.createLayer('Camada de Tiles 1');
+        layer.resizeWorld();
+        map.setCollisionBetween(0, 2, true, 'Camada de Tiles 1');
+
+        hero = game.add.sprite(10,game.world.height - 200,'hero');
         game.physics.enable(hero, Phaser.Physics.ARCADE);
 
         hero.body.collideWorldBounds = true;
+    },
+
+    update:function(){
+        game.physics.arcade.collide(layer, hero);
     }
 
 }
