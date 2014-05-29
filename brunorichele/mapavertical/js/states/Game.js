f5e15df16d04ca4a3f7fbbe969db11c5e6bd570c
Bruno Araujo <bruno.araujo@gmail.com>
2014-05-29 00:10:05 -0400

    Corrigindo carregamento de tiles em mapavertical

--- brunorichele/mapavertical/js/states/Game.js
+++ brunorichele/mapavertical/js/states/Game.js
@@ -12,8 +12,9 @@ State.Game.prototype = {
     create: function() {
         "use strict";
         var map = game.add.tilemap('tilemap');
-        map.addTilesetImage('tileset', 'tileset');
-
+        // ordem correta: nome do tileset no Tiled, nome do asset no Phaser
+        // nesse caso o nome do tileset é texturebreakout, e o nome do assed é tileset
+        map.addTilesetImage('texturebreakout', 'tileset');
         var layer = map.createLayer('Camada de Tiles 1');
         layer.resizeWorld();
     },
