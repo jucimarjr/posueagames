2e6dde951c99ef9d301f594761a794c77a68449d
diegonobu <diegonobu@gmail.com>
2014-05-30 19:54:41 -0400

    heroi atira

--- felipenoir/plataforma/js/entities/Hero.js
+++ felipenoir/plataforma/js/entities/Hero.js
@@ -1,10 +1,22 @@
 var Hero = {
-    path:'assets/images/hero/hero_50-50-4.png',
+    path:'assets/images/hero/h.png',
+    json:'assets/images/hero/h.json',
     width:50,
     height:50,
-    frames:4,
     dragX:200,
     gravityY:100,
     velocityX:100,
-    jump:-350
+    jump:-350,
+    animations:{
+        run:{
+            name:'run',
+            frames:[1,2,3]
+        }
+    }
 };
+
+var Weapon = {
+    shot_delay:1000,
+    bullet_speed:500,
+    number_of_bullets:20
+}
--- felipenoir/plataforma/js/states/Play.js
+++ felipenoir/plataforma/js/states/Play.js
@@ -1,13 +1,14 @@
 State.Play = function() {
-    var map, layer, hero, cursors;
+    var map, layer, hero, cursors, bulletGroup;
 };
 
 State.Play.prototype = {
     preload:function() {
-        game.load.atlasJSONHash('hero', 'assets/images/hero/h.png', 'assets/images/hero/h.json');
+        game.load.atlasJSONHash('hero', Hero.path, Hero.json);
         game.load.tilemap('map', Level.tilemap.jsonPath, null, Phaser.Tilemap.TILED_JSON);
         game.load.image('tileset', Level.tilemap.tilePath);
         game.load.image('bg', Level.bg.path);
+        game.load.image('bullet', 'assets/images/hero/bullet.png');
         //game.load.spritesheet('hero', Hero.path, Hero.width, Hero.height, Hero.frames);
     },
 
@@ -27,7 +28,7 @@ State.Play.prototype = {
 
         //hero = game.add.sprite(10,game.world.height - 200,'hero', 0);
         hero = game.add.sprite(10,game.world.height - 200,'hero');
-        hero.animations.add('run', [1,2,3], 6, true);
+        hero.animations.add(Hero.animations.run.name, Hero.animations.run.frames, 6, true);
         hero.anchor.setTo(.5,.5);
         game.physics.enable(hero, Phaser.Physics.ARCADE);
 
@@ -35,6 +36,15 @@ State.Play.prototype = {
         hero.body.drag.x = Hero.dragX;
         hero.body.gravity.y = Hero.gravityY;
         game.camera.follow(hero);
+
+        bulletGroup = game.add.group();
+        for(var i = 0; i < Weapon.number_of_bullets; i++) {
+            bullet = game.add.sprite(0,0,'bullet');
+            bulletGroup.add(bullet);
+            bullet.anchor.setTo(.5,.5);
+            game.physics.enable(bullet, Phaser.Physics.ARCADE);
+            bullet.kill();
+        }
 
         cursors = game.input.keyboard.createCursorKeys();
     },
@@ -45,14 +55,18 @@ State.Play.prototype = {
         if (cursors.right.isDown) {
             hero.body.velocity.x = Hero.velocityX;
             hero.scale.x = +1;
-            hero.animations.play('run');
+            hero.animations.play(Hero.animations.run.name);
         } else if (cursors.left.isDown) {
             hero.body.velocity.x = -Hero.velocityX;
             hero.scale.x = -1;
-            hero.animations.play('run');
+            hero.animations.play(Hero.animations.run.name);
         } else {
             hero.animations.stop();
             hero.frame = 0;
+        }
+
+        if(game.input.keyboard.isDown(Phaser.Keyboard.Z)) {
+            this.shoot();
         }
 
         if(cursors.up.isDown) {
@@ -60,6 +74,26 @@ State.Play.prototype = {
                 hero.body.velocity.y = Hero.jump;
             }
         }
+    },
+
+    shoot:function(){
+        if(this.lastBulletShotAt === undefined) this.lastBulletShotAt = 0;
+        if(game.time.now - this.lastBulletShotAt < Weapon.shot_delay) return;
+
+        bullet = bulletGroup.getFirstDead();
+
+        if(bullet === null || bullet === undefined) return;
+
+        bullet.revive();
+
+        bullet.checkWorldBounds = true;
+        bullet.outOfBoundsKill = true;
+
+        bullet.reset(hero.x, hero.y);
+
+        bullet.body.velocity.x = Weapon.bullet_speed;
+        bullet.body.velocity.y = 0;
+        bullet.body.allowGravity = false;
     }
 
 }
