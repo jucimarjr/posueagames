2158c9a8cc7b0f4006abcc95898cbb1e36f7785d
diegonobu <diegonobu@gmail.com>
2014-05-31 21:07:06 -0400

    mudando codigo do heroi de lugar

--- felipenoir/plataforma/js/entities/Hero.js
+++ felipenoir/plataforma/js/entities/Hero.js
@@ -1,4 +1,4 @@
-var Hero = {
+var HeroPropeties = {
     path:'assets/images/hero/h.png',
     json:'assets/images/hero/h.json',
     width:50,
@@ -14,4 +14,51 @@ var Hero = {
             frames:[1,2,3]
         }
     }
-};
+}
+
+function Hero(game) {
+    this.game = game,
+    this.hero,
+    this.cursors;
+}
+
+Hero.prototype = {
+    preload : function() {
+        this.game.load.atlasJSONHash('hero', HeroPropeties.path, HeroPropeties.json);
+    },
+
+    create : function() {
+        this.hero = game.add.sprite(10,game.world.height - 200,'hero');
+        this.hero.animations.add(HeroPropeties.animations.run.name, HeroPropeties.animations.run.frames, 6, true);
+        this.hero.anchor.setTo(.5,.5);
+        this.game.physics.enable(this.hero, Phaser.Physics.ARCADE);
+
+        this.hero.body.collideWorldBounds = true;
+        this.hero.body.drag.x = HeroPropeties.dragX;
+        this.hero.body.gravity.y = HeroPropeties.gravityY;
+        this.game.camera.follow(this.hero);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    },
+
+    update : function() {
+        if (this.cursors.right.isDown) {
+            this.hero.body.velocity.x = HeroPropeties.velocityX;
+            this.hero.scale.x = +1;
+            this.hero.animations.play(HeroPropeties.animations.run.name);
+        } else if (this.cursors.left.isDown) {
+            this.hero.body.velocity.x = -HeroPropeties.velocityX;
+            this.hero.scale.x = -1;
+            this.hero.animations.play(HeroPropeties.animations.run.name);
+        } else {
+            this.hero.animations.stop();
+            this.hero.frame = 0;
+        }
+
+        if(this.cursors.up.isDown) {
+            if(this.hero.body.onFloor()) {
+               this.hero.body.velocity.y = HeroPropeties.jump;
+            }
+        }
+    }
+}
--- felipenoir/plataforma/js/states/Play.js
+++ felipenoir/plataforma/js/states/Play.js
@@ -1,15 +1,16 @@
 State.Play = function() {
-    var map, layer, hero, cursors, bulletGroup;
-    this.weapon = new Weapon(game);
+    var map, layer;
+    this.weapon = new Weapon(game),
+    this.hero = new Hero(game);
 };
 
 State.Play.prototype = {
     preload:function() {
-        game.load.atlasJSONHash('hero', Hero.path, Hero.json);
         game.load.tilemap('map', Level.tilemap.jsonPath, null, Phaser.Tilemap.TILED_JSON);
         game.load.image('tileset', Level.tilemap.tilePath);
         game.load.image('bg', Level.bg.path);
         this.weapon.preload();
+        this.hero.preload();
     },
 
     create:function(){
@@ -29,46 +30,16 @@ State.Play.prototype = {
         map.setCollisionBetween(Level.tilemap.collisionStart, Level.tilemap.collisionEnd, true, Level.tilemap.layer);
 
         // heroi
-        hero = game.add.sprite(10,game.world.height - 200,'hero');
-        hero.animations.add(Hero.animations.run.name, Hero.animations.run.frames, 6, true);
-        hero.anchor.setTo(.5,.5);
-        game.physics.enable(hero, Phaser.Physics.ARCADE);
-
-        hero.body.collideWorldBounds = true;
-        hero.body.drag.x = Hero.dragX;
-        hero.body.gravity.y = Hero.gravityY;
-        game.camera.follow(hero);
+        this.hero.create();
 
         // arma
         this.weapon.create();
-
-        // setas do teclado
-        cursors = game.input.keyboard.createCursorKeys();
     },
 
     update:function(){
-        game.physics.arcade.collide(layer, hero);
-
-        if (cursors.right.isDown) {
-            hero.body.velocity.x = Hero.velocityX;
-            hero.scale.x = +1;
-            hero.animations.play(Hero.animations.run.name);
-        } else if (cursors.left.isDown) {
-            hero.body.velocity.x = -Hero.velocityX;
-            hero.scale.x = -1;
-            hero.animations.play(Hero.animations.run.name);
-        } else {
-            hero.animations.stop();
-            hero.frame = 0;
-        }
-
-        this.weapon.update(hero.x, hero.y);
-
-        if(cursors.up.isDown) {
-            if(hero.body.onFloor()) {
-                hero.body.velocity.y = Hero.jump;
-            }
-        }
+        game.physics.arcade.collide(layer, this.hero.hero);
+        this.hero.update();
+        this.weapon.update(this.hero.hero.x, this.hero.hero.y);
     },
 
 }
