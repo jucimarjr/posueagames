75e7140c3d9cdab1257613326a2d863479307633
josemdvdf <josemdvdf@gmail.com>
2014-06-02 13:58:47 -0400

    secondphase

--- johnsilva/canoeirobarev2/js/states/secondPhase.js
+++ johnsilva/canoeirobarev2/js/states/secondPhase.js
@@ -1,26 +1,30 @@
+/*global State, Config*/
+
 State.SecondPhase = function (game) {
-		"use strict";
-		this.game = game;
+	"use strict";
+	this.game = game;
+	//	var player;
 };
 State.SecondPhase.prototype = {
 	preload: function () {
 		"use strict";
 		/*game.load.tilemap('stage', 'assets/map2.json', null, Phaser.Tilemap.TILED_JSON);
-		game.load.image('background', 'assets/bg.png');
+		
 		game.load.image('tileset', 'assets/tileset.png');
-		game.load.spritesheet('mario', 'assets/mario.png', 36,54,5);
 		game.load.image('star', 'assets/star.png');*/
+		//this.game.load.image('background', 'assets/LudusSplash_960-600.png');
+		//this.game.load.image('background', 'assets/bg.png');
+		//this.game.load.spritesheet('player', 'assets/mario.png', 180,54,5);
 	},
 	create: function () {
 		"use strict";
 
-		/*game.physics.startSystem(Phaser.Game.ARCADE);
-		game.physics.arcade.gravity.y = 800;
+		this.game.physics.startSystem(Phaser.Game.ARCADE);
+		this.game.physics.arcade.gravity.y = 800;
+		var bg = this.game.add.tileSprite(0,-600,2000,1200,'background');
+		bg.fixedToCamera = true;
 	
-		this.bg = game.add.tileSprite(0,-600,2000,1200,'background');
-		this.bg.fixedToCamera = true;
-	
-		this.map = game.add.tilemap('stage');
+		/*this.map = game.add.tilemap('stage');
 		this.map.addTilesetImage('tileset','tileset');
 	
 		this.layer = this.map.createLayer('Camada de Tiles 1');
@@ -30,56 +34,57 @@ State.SecondPhase.prototype = {
 		group = game.add.group();
 		group.enableBody = true;
 		this.map.createFromObjects('Camada de Objetos 1',17, 'star', 0,true,false,group);
-		group.forEach(function (star){ star.body.allowGravity = false}, this);
+		group.forEach(function (star){ star.body.allowGravity = false}, this);*/
 
-		game.camera.y = 1000;
-		this.sprite = game.add.sprite(10,1000 ,'mario', 3);
-		this.sprite.anchor.setTo(.5, 1);
-		game.physics.enable(this.sprite);
-		this.sprite.smoothed = false; 
-		this.sprite.body.checkCollision.up = false;
-		this.sprite.body.checkCollision.left = false;
-		this.sprite.body.checkCollision.right = false;
-		this.sprite.animations.add('walk',[1,2,1,3],12,true);
-		this.sprite.animations.add('jump',[4],1,false);
-		this.sprite.animations.add('stop',[0],1,false);
-		this.sprite.body.collideWorldBounds = true;
-		game.camera.follow(this.sprite);
-		this.cursors = game.input.keyboard.createCursorKeys();
-		this.jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);	*/	
+		//this.game.camera.y = 1000;
+		//player = game.add.sprite(10,100 ,'player');
+		//player.anchor.setTo(.5, 1);
+		//game.physics.enable(this.sprite);
+		//player.smoothed = false;
+		//player.body.checkCollision.up = false;
+		//player.body.checkCollision.left = false;
+		//player.body.checkCollision.right = false;
+		//player.animations.add('walk',[1,2,1,3],12,true);
+		/*player.animations.add('walk',[1,2],12,true);
+		player.animations.add('down',[3,4],12,true);
+		player.animations.add('jump',[1],1,false);
+		player.animations.add('stop',[0],1,false);
+		player.animations.play('walk');*/
+		//player.body.collideWorldBounds = true;
+		/*game.camera.follow(this.sprite);
+		var cursors = game.input.keyboard.createCursorKeys();
+		var jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);*/
 	},
 	update: function () {
-		/*"use strict";
-		Config.global.screen.resize(this.game);
+		"use strict";
+		//Config.global.screen.resize(this.game);
+		//player.animations.play('walk');
 
-		game.physics.arcade.collide(this.layer, this.sprite);
+		/*game.physics.arcade.collide(this.layer, this.sprite);
 		game.physics.arcade.overlap(group, this.sprite, collision, null,this);
-
-		this.sprite.body.velocity.x = 0;
+		*/
+		/*player.body.velocity.x = 0;
 		if(this.cursors.left.isDown){
-			this.sprite.scale.x = 1; 
-			this.sprite.scale.x = -1;
-			this.sprite.animations.play('walk');		
-			this.sprite.body.velocity.x = -150;
+			player.scale.x = 1; 
+			player.scale.x = -1;
+			player.animations.play('walk');		
+			player.body.velocity.x = -150;
 		}
 		else if(this.cursors.right.isDown){
-			this.sprite.scale.x = -1; 
-			this.sprite.scale.x = 1;
-			this.sprite.animations.play('walk');		
-			this.sprite.body.velocity.x = 150;
+			player.scale.x = -1; 
+			player.scale.x = 1;
+			player.animations.play('walk');		
+			player.body.velocity.x = 150;
 		}
-		else if(this.sprite.body.blocked.down){
-			this.sprite.animations.stop();
-			this.sprite.frame = 0;
+		else if(player.body.blocked.down){
+			player.animations.stop();
+			player.frame = 0;
 		}
-		if(this.sprite.body.velocity.y !== 0){
-			this.sprite.animations.play('jump');
+		if(player.body.velocity.y !== 0){
+			player.animations.play('jump');
 		}
-		if (this.jumpButton.isDown && this.sprite.body.onFloor()){
-			this.sprite.body.velocity.y = -450;
+		if (this.jumpButton.isDown && player.body.onFloor()){
+			player.body.velocity.y = -450;
 		}*/
-	},
-	function collision (sprite,star){
-		/*star.kill();*/
 	}
 };
