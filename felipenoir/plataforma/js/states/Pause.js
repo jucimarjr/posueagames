46c335394c4257c8c7633cb2f371874cb78c0b27
unknown <marcos.castro@marcos-castro.dom.fpf.br>
2014-06-03 08:26:20 -0400

    menu sempre aparece no centro da tela

--- felipenoir/plataforma/js/states/Pause.js
+++ felipenoir/plataforma/js/states/Pause.js
@@ -3,8 +3,6 @@ var Pause = function(game){
 	this.game = game;
 	this.imageMenu = 'assets/images/menu.png';
 	this.menu;
-	this.w = 960;
-	this.h = 600;
 };
 
 Pause.prototype = {
@@ -21,12 +19,14 @@ Pause.prototype = {
 			this.pauseGame();
 		}
 	},
-	pauseGame : function (){
+	pauseGame : function (){		
 		this.game.paused = true;
-		this.menu = game.add.sprite(this.w/2, this.h/2, 'menu');
+		var moduloPosition = this.game.world.position.x < 0?(-1)*this.game.world.position.x : this.game.world.position.x; 
+		this.menu = game.add.sprite(moduloPosition + this.game.width/2, this.game.height/2, 'menu');
 		this.menu.anchor.setTo(0.5, 0.5);
 	},
 	resumeGame : function (event){
+		console.log(event.game);
 		if(this.game.paused){
 			this.menu.destroy();
 			this.game.paused = false;
