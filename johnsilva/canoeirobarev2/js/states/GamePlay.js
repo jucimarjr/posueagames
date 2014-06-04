dea1cabe325a61f49157139b6d9d23f18b5fbc28
josemdvdf <josemdvdf@gmail.com>
2014-06-04 14:08:12 -0400

    morrer

--- johnsilva/canoeirobarev2/js/states/GamePlay.js
+++ johnsilva/canoeirobarev2/js/states/GamePlay.js
@@ -3,6 +3,7 @@
 State.GamePlay = function (game) {
 	"use strict";
 	this.game = game;
+	var deadAnimation;
 	/*var player;
 	var level;
 	var map;
@@ -18,6 +19,7 @@ State.GamePlay.prototype = {
 		this.game.physics.startSystem(Phaser.Game.ARCADE);
 		this.game.physics.arcade.gravity.y = 100;
 		this.game.stage.smoothed = false;
+		this.level = 3;
 		//this.game.world.setBounds(0, -600, 1600, 1200);
 
 		this.player = game.add.sprite(10,1000 ,'playerS');
@@ -30,18 +32,20 @@ State.GamePlay.prototype = {
 		//player.animations.add('walk',[1,2,1,3],12,true);
 		this.player.animations.add('walk',[3,4,5,6,7,8,9,10,11,12,13,14],15,true);
 		this.player.animations.add('stoped',[0,1],2,true);
-		this.player.animations.add('down',[3,4],12,true);
-		this.player.animations.add('jump',[15,16,17,18,19],6,false);
-		this.game.physics.enable(this.player);
-		//player.animations.play('walk');
-		this.player.body.collideWorldBounds = true;
-		//this.game.camera.follow(player);
-		this.game.physics.enable(this.player);
-		this.player.body.setSize(20, 60, 0, 0);
+		this.player.animations.add('down',[3,4],12,true);		
+		this.player.animations.add('dead',[24,25,26],10,false);
+		/*deadAnimation.onComplete.add(function() {
+	    								this.loadLevel(this.level);
+									}, this);	*/
+		this.player.animations.add('jump',[15,16,17,18,19],4,false);
 
-		this.loadLevel(3);
-	
 		
+
+		this.game.physics.enable(this.player);
+		this.player.body.collideWorldBounds = true;
+		this.game.physics.enable(this.player);
+		this.player.body.setSize(25, 60, 0, 0);
+		this.loadLevel(this.level);
 		this.game.camera.y = 1000;
 		
 		//cursors = this.game.input.Keyboardboard.createCursorKeys();
@@ -65,6 +69,9 @@ State.GamePlay.prototype = {
 		this.spaceBar.onUp.add(function() {this.jump=false;}, this);*/
 	},
 	update: function () {
+
+		//if(!this.gameOver){
+		
 		Config.global.screen.resize(this.game);
 		//player.animations.play('walk');
 
@@ -76,24 +83,27 @@ State.GamePlay.prototype = {
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) ) {
         //if (this.back){
 			this.player.scale.x = 1; 
-			this.player.scale.x = -1;
-			this.player.animations.play('walk');		
+			this.player.scale.x = -1;			
 			this.player.body.velocity.x = -150;
 			this.crouched = false;
+			if(this.player.body.onFloor()){
+				this.player.animations.play('walk');
+			}
 			//this.bg.tilePosition.x+=1;
 		}
 		else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
 		//else if(this.run){
 			this.player.scale.x = -1; 
 			this.player.scale.x = 1;
-			this.player.animations.play('walk');
 			this.player.body.velocity.x = 150;
 			this.crouched = false;
-
+			if(this.player.body.onFloor()){
+				this.player.animations.play('walk');
+			}
 			//this.bg.tilePosition.x-=1;
 		}
 		else if(this.player.body.blocked.down){
-			this.player.animations.stop();
+			//this.player.animations.stop();
 			//this.player.frame = 0;
 			this.player.animations.play('stoped');
 		}
@@ -107,9 +117,19 @@ State.GamePlay.prototype = {
 			this.player.animations.play('jump');
 			this.player.body.velocity.y = -450;
 		}
+	//}
 	},
 
 	loadLevel: function (level) {
+		this.player.alpha = 1;
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
+		this.player.position.setTo(75, 650);
+		this.player.body.gravity.y = 800;
+		this.game.camera.follow(this.player);
+
+		this.level = level;
+
 		if (this.layer) this.layer.destroy();
 		//if (this.flag) this.flag.destroy();
 		if (this.bees) this.bees.destroy();
@@ -139,12 +159,12 @@ State.GamePlay.prototype = {
 		}, this);
 
 		this.layer = this.map.createLayer('Camada de Tiles 1');
-		this.layer.resizeWorld();		
+		this.layer.resizeWorld();
+		this.game.physics.enable(this.layer);
 
 		this.bees = game.add.group();
 		this.bees.enableBody = true;
 		this.bees.physicsBodyType = Phaser.Physics.ARCADE;
-
 		this.map.createFromObjects('Camada de Objetos 1',4,'bee', 0,true,false,this.bees);
 		this.bees.callAll('animations.add', 'animations', 'spin', [0, 1], 3, true);
     	this.bees.callAll('animations.play', 'animations', 'spin');
@@ -154,28 +174,14 @@ State.GamePlay.prototype = {
 		}, this);
 
 
-
 		this.tubes = game.add.group();
 		this.tubes.enableBody = true;
 		this.tubes.physicsBodyType = Phaser.Physics.ARCADE;
-
 		this.map.createFromObjects('tubes',6,'tube', 0,true,false,this.tubes);
 		this.tubes.forEach(function (tube){ 
 			tube.body.allowGravity = false;
 			tube.body.immovable = true;
 		}, this);
-
-		
-
-		this.game.physics.enable(this.layer);
-
-		this.player.body.velocity.x = 0;
-		this.player.body.velocity.y = 0;
-		this.player.position.setTo(100, this.game.world.height - 300);
-		this.player.body.gravity.y = 800;
-
-		this.game.camera.follow(this.player);
-		this.level = level;
 		//this.addEmmiter(500,500);
 	},
 
@@ -205,18 +211,23 @@ State.GamePlay.prototype = {
 	},
 
 	die : function(player, bee) {
-	this.player.anchor.setTo(0.5, 0.5);
-	var t = this.game.add.tween(this.player).to({angle:360}, 300).start();
-	this.player.body.gravity.y = 0;
-	this.player.body.velocity.x = 0;
-	this.player.body.velocity.y = 0;
-	t.onComplete.add(function() {
-	    this.loadLevel(this.level);
-	}, this);
+		//this.gameOver = true;
+		//this.player.anchor.setTo(0.5, 0.5);
+		this.player.animations.play('dead');
+		var t = this.game.add.tween(this.player).to({alpha:0}, 1000).start();
+		bee.kill();	
+		this.player.body.gravity.y = 0;
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
+	
+		t.onComplete.add(function() {
+		    this.loadLevel(this.level);
+		}, this);
     },
 
+    //game
+
     render: function (){
-    	game.debug.body(this.player);
-    	game.debug.body(this.bees);
+    	//game.debug.body(this.player);
     },
 };
