750a6f3d19bbaa4ed2f8a6a1329375f56b81dde2
petrina8 <petri.kim8@gmail.com>
2014-06-03 20:51:21 -0400

    change to P2 physics

--- renanbrg/dropWater/js/model/Character.js
+++ renanbrg/dropWater/js/model/Character.js
@@ -99,7 +99,9 @@ Character.prototype = {
      * @param {integer} speed - how fast the sprite will move to the right.
      */
     moveRight: function(speed) {
-        this.character.x += speed;
+    	//console.log("velocidade: "+this.character.x);
+        //this.character.x += speed;
+        //console.log("velocidade: "+this.character.x);
         if (this.isAnimated) {
             this.character.animations.play('right');
         }
@@ -114,11 +116,10 @@ Character.prototype = {
      * @param {integer} speed - how fast the sprite will move to the left.
      */
     moveLeft: function(speed) {
-        this.character.x -= speed;
+        //this.character.x -= speed;
         if (this.isAnimated) {
         	this.character.animations.play('left');
         }
- 
     },
 
     /**
@@ -144,9 +145,10 @@ Character.prototype = {
      * It must be a value bigger than 0.
      */
     jump: function(jumpHeight) {
-        if (this.character.body.onFloor() || this.character.body.touching.down) {
+        //if (this.character.body.onFloor() || this.character.body.touching.down) {
+    	//if (this.character.body.touching.down)
             this.character.body.velocity.y = -jumpHeight;
-        }
+       // }
     },
 
     /**
--- renanbrg/dropWater/js/states/GamePlay.js
+++ renanbrg/dropWater/js/states/GamePlay.js
@@ -7,6 +7,10 @@ State.GamePlay = function (game) {
     this.layer = null;
     this.player = null;
     this.crabs = null;
+    this.dropCollisionGroup = null;
+    this.crabCollisionGroup = null;
+    this.layerBody = null;
+
     try {
         this.drop = new Character(this.game, 'dude',
                 'assets/images/Dude_32-48.png', [32, 48]);
@@ -21,6 +25,7 @@ State.GamePlay.prototype = {
 		this.game.load.tilemap('map', 'assets/mapLevel1_960-600.json', null, Phaser.Tilemap.TILED_JSON);
 	    this.game.load.image('tileset','assets/images/tileset.png');
 	    this.game.load.image('crab','assets/images/crab_80-80.png');
+	    
 	    this.game.load.audio('jump','assets/waterDrop.mp3');
 
 		// Player
@@ -31,87 +36,62 @@ State.GamePlay.prototype = {
 		var background;
 		background = this.game.add.tileSprite(Config.gamePlay.x, Config.gamePlay.y, game.world.width, game.world.height, 'gameplay-bg');
 		background.fixedToCamera = true;
+
+		this.game.physics.startSystem(Phaser.Physics.P2JS);
+        this.game.physics.p2.gravity.y = 800;
 		
 		this.map = this.game.add.tilemap('map');
-		this.map.addTilesetImage('tileset','tileset');
+		this.map.addTilesetImage('tileset');
 		this.layer = this.map.createLayer('Camada de Tile 1');
         this.layer.resizeWorld();
-        this.map.setCollisionBetween(1, 3, true,'Camada de Tile 1');
         
-        //Sounds
-        this.jumpSound = this.game.add.audio("jump");
-
-		// Creating player
-        this.drop.create(300, game.world.height-200);
-
-        // Remember that you first have to define the physics of the sprite
-        // to be able to set values in the body property.
-        var dropSprite = this.drop.getSpriteObject();        
-        this.game.physics.enable(dropSprite, Phaser.Physics.ARCADE);
-        this.drop.configureCharacter(this.setCharacterInicialValues);
-
+        //  Set the tiles for collision.
+        //  Do this BEFORE generating the p2 bodies below.
+        this.map.setCollisionBetween(1, 3);                
+        this.game.physics.p2.convertTilemap(this.map, this.layer);
+                
+        // create player
+        this.drop.create(300, this.game.world.height-200);
+        var dropSprite = this.drop.getSpriteObject();   
+        this.game.physics.p2.enable(dropSprite);
         this.game.camera.follow(dropSprite);
-        this.game.camera.x += 600;
+        this.drop.configureCharacter(this.setCharacterInicialValues);
         
-        // add enemy crab
-        this.crabs = this.game.add.group();
-        this.crabs.create(this.game.width+130, this.game.height-160, 'crab');
-        this.crabs.create(this.game.width+210, this.game.height-160, 'crab');
-        this.crabs.enableBody = true;
-        this.game.physics.enable(this.crabs, Phaser.Physics.ARCADE);
-        this.crabs.setAll("body.immovable", true); //dont move when it collides        
-        this.crabs.forEach(function (item){ 
-            item.body.collideWorldBounds = true;
-            item.body.checkCollision.up = true;
-            item.body.checkCollision.left = true;
-            item.body.checkCollision.right = true;            
-            item.body.bounce.x = 0.8;            
-        }, this);
-        // to left side
-        this.crabs.getAt(0).body.velocity.x = -100;
-        // to right side
-        this.crabs.getAt(1).body.velocity.x = 100;        
-        
+        // Sounds
+        this.jumpSound = this.game.add.audio("jump");
+                
     },
     setCharacterInicialValues: function(character) {    	
     	character.smoothed = false;
-    	character.body.checkCollision.up = true;
-    	character.body.checkCollision.left = true;
-    	character.body.checkCollision.right = true;
-    	character.body.gravity.y = 800;
-        character.scale.set(1.5, 1.5);        
-        character.body.collideWorldBounds = true;        
+        character.body.collideWorldBounds = true;    
+        character.body.fixedRotation = true;
         character.animations.add('left', [0, 1, 2, 3], 10, true);
         character.animations.add('right', [5, 6, 7, 8], 10, true);
     },
 	update: function () {
 		"use strict";
-        this.game.physics.arcade.collide(this.drop.getSpriteObject(), this.layer); 
-        this.game.physics.arcade.collide(this.crabs, this.layer);
-        this.game.physics.arcade.collide(this.drop.getSpriteObject(), this.crabs, this.crabKillDrop, null, this); //collide with crabs and drop                
-        this.game.physics.arcade.overlap(this.crabs.getAt(0), this.crabs.getAt(1), this.moveCrab, null, this); //collide crab with crab
-                        
 		this.handleKeyDown();
 	},
 	handleKeyDown: function () {
 		"use strict";
-
-		this.drop.setVelocityX(0);
+		//this.drop.getSpriteObject().body.setZeroVelocity();
 
 		if ( this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) ) {
             this.drop.moveRight(4);
+			this.drop.getSpriteObject().body.moveRight(150);
 
 		} else if ( this.game.input.keyboard.isDown (Phaser.Keyboard.LEFT) ) {
             this.drop.moveLeft(4);
+            this.drop.getSpriteObject().body.moveLeft(150);
 
 		} else {
             this.drop.stop();
 		}
 		// Jump
 		if ( this.game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR) ) {
-            this.drop.jump(450);
-            if ( this.game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR)&& (this.drop.getSpriteObject().body.onFloor())) 
-            this.jumpSound.play();
+            this.drop.jump(450);  
+            //if ( this.game.input.keyboard.isDown (Phaser.Keyboard.SPACEBAR)&& (this.drop.getSpriteObject().body.onFloor())) 
+				this.jumpSound.play();          
 		}
 	},
 	moveCrab: function () {
@@ -121,12 +101,12 @@ State.GamePlay.prototype = {
 		this.crabs.getAt(1).body.velocity.x = 100;		
 	},
 	crabKillDrop: function () {
-		if (this.drop.getSpriteObject().body.onFloor() 
-				&& (this.drop.getSpriteObject().body.touching.left 
-				|| this.drop.getSpriteObject().body.touching.right)) {
+		//if (this.drop.getSpriteObject().body.onFloor() 
+				//&& (this.drop.getSpriteObject().body.touching.left 
+				//|| this.drop.getSpriteObject().body.touching.right)) {
 			
 			this.drop.getSpriteObject().kill();		
-		}
+		//}
 	},
 	clickHowToPlay: function () {
 		"use strict";
