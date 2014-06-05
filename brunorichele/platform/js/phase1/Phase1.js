45469ea3a27ed8a508f783c27994ee73992ae910
Bruno Araujo <bruno.araujo@gmail.com>
2014-06-04 22:01:14 -0400

    Adicionando a velocidade do jogador

--- brunorichele/platform/js/phase1/Phase1.js
+++ brunorichele/platform/js/phase1/Phase1.js
@@ -65,17 +65,17 @@ State.Phase1.prototype = {
         Config.global.screen.resize(this.game);
         this.game.physics.arcade.collide(this.layer, this.player, this.wallCollision, null, this);
         if(this.cursors.left.isDown){
-            this.player.body.velocity.x = -100;
+            this.player.body.velocity.x = -this.player.velocity;
         }
         else if(this.cursors.right.isDown){
-            this.player.body.velocity.x = 100;
+            this.player.body.velocity.x = this.player.velocity;
         }
         else this.player.body.velocity.x = 0;
         if(this.cursors.up.isDown){
-            this.player.body.velocity.y = -100;
+            this.player.body.velocity.y = -this.player.velocity;
         }
         else if(this.cursors.down.isDown){
-            this.player.body.velocity.y = 100;
+            this.player.body.velocity.y = this.player.velocity;
         }
         else this.player.body.velocity.y = 0;
 
@@ -85,6 +85,7 @@ State.Phase1.prototype = {
         this.player = this.game.add.sprite(0, 300, 'jogador');
         this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.body.collideWorldBounds = true;
+        this.player.velocity = 300;
     },
 
     wallCollision : function (){
