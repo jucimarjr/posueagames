1821fa43759526c9c46bfe5f25afffb09d0568de
HelderBatista <hcb007@gmail.com>
2014-06-05 00:06:47 -0400

    Correcao do bug

--- lenasottomayor/Blockbuster/js/game/Coins.js
+++ lenasottomayor/Blockbuster/js/game/Coins.js
@@ -12,6 +12,6 @@ Coins.prototype = {
 		this.group = game.add.group();
 		this.group.enableBody = true;
 		this.tilemap.map.createFromObjects(Config.coins.name, Config.coins.gid, 'coin', Config.coins.frame,true,false,this.group);
-		this.group.forEach(function (coin){ coin.body.allowGravity = false}, this);
+		this.group.forEach(function (coin){ coin.body.allowGravity = false;}, this);
 	}
 };
