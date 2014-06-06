12a5e73860d3853458da3d2f337ca4c2476074de
Rodrigo Braga <rodrigogrow@gmail.com>
2014-06-06 08:15:35 -0400

    add menu view

--- rageloop/ninjs/js/states/menu.js
+++ rageloop/ninjs/js/states/menu.js
@@ -6,9 +6,26 @@
 
         create: function () {
             this.stage.backgroundColor = '#ccc';
+
+            this.game.add.sprite(0, 0, 'menu_bg');
+
+            this.startbutton = this.game.add.button(this.game.world.centerX - 120, this.game.world.centerY + 150, 'start_btn', this.onPlayClick, this, 0, 1, 0);
+            this.startbutton.anchor.set(0.5, 0.5);
+
+            this.resetbutton = this.game.add.button(this.game.world.centerX + 120, this.game.world.centerY + 150, 'reset_btn', this.onResetClick, this, 1, 0, 1);
+            this.resetbutton.anchor.set(0.5, 0.5);
+
         },
 
-        update: function() {},        
+        update: function() {},
+
+        onPlayClick: function() {
+            this.game.state.start('Gameplay');
+        },
+
+        onResetClick: function() {
+            //this.game.state.start('Gameplay');
+        },
     };
 
     app_container.Menu = Menu;
--- rageloop/ninjs/js/states/preload.js
+++ rageloop/ninjs/js/states/preload.js
@@ -4,6 +4,13 @@
 
     Preload.prototype = {
         preload: function () {
+
+            /*Menu assets*/
+            this.game.load.image('menu_bg','assets/images/menuBg_960-600.png');
+            this.game.load.spritesheet('start_btn','assets/sprites/start_btn_199-114.png', 199, 57);
+            this.game.load.spritesheet('reset_btn','assets/sprites/reset_btn_199-114.png', 199, 57);
+
+            /*Gameplay assets*/
             this.game.load.spritesheet('ninja', 'assets/sprites/ninja_475-34-16.png', 22, 34);
             this.game.load.tilemap('map', 'assets/maps/map01.json', null, Phaser.Tilemap.TILED_JSON);
             this.game.load.image('tileset','assets/sprites/tileset.png');
@@ -11,7 +18,7 @@
         },
         create: function () {},
         update: function() {
-            this.game.state.start('Gameplay');
+            this.game.state.start('Menu');
         }
     };
 
