42c63b8266b225fe01bbf3fe1015f2548c8df926
Bruno Campos <bruno.campos@openbossa.org>
2014-06-06 12:13:19 -0400

    Add drop bar
    Add a sprite sheet for the drop bar and methods for increase and decrease
    the drop bar when the player gets a new drop.

--- renanbrg/dropWater/js/model/HUD.js
+++ renanbrg/dropWater/js/model/HUD.js
@@ -19,6 +19,7 @@ HUD = function(game) {
     this.xLabel = null;
     this.lifeLabel = null;
     this.lifeCounter = 3;
+    this.dropCounter = 0;
 };
 
 HUD.prototype = {
@@ -32,6 +33,8 @@ HUD.prototype = {
         "use strict";
 
         this.game.load.image('life_image', 'assets/images/life_40-40.png');
+        this.game.load.spritesheet('dropAttempts',
+                'assets/images/drop_attempts_1000-200.png', 40, 200);
     },
 
     /**
@@ -54,9 +57,49 @@ HUD.prototype = {
         var styleForLife = { font: "40px Helvetica", fill: "#ffffff" };
         this.lifeLabel = game.add.text(90, 22, this.lifeCounter, styleForLife);
         this.lifeLabel.fixedToCamera = true;
+
+        this.dropAttemps = this.game.add.sprite(Config.global.screen.width - 60,
+                20, 'dropAttempts');
+        this.dropAttemps.animations.add('attempt1Up',
+                [0, 1, 2, 1, 2, 3, 2, 3, 4], 10,
+                false);
+        this.dropAttemps.animations.add('attempt1Down',
+                [4, 3, 2, 3, 2, 1, 2, 1, 0], 10, false);
+        this.dropAttemps.animations.add('attempt2Up',
+                [4, 5, 6, 5, 6, 7, 6, 7, 8, 7, 8, 9], 10, false);
+        this.dropAttemps.animations.add('attempt2Down',
+                [9, 8, 7, 8, 7, 6, 7, 6, 5, 6, 5, 4], 10, false);
+        this.dropAttemps.animations.add('attempt3Up',
+                [9, 10, 11, 10, 11, 12, 11, 12, 13, 12, 13, 14], 10, false);
+        this.dropAttemps.animations.add('attempt3Down',
+                [14, 13, 12, 13, 12, 11, 12, 11, 10, 11, 10, 9], 10, false);
+        this.dropAttemps.animations.add('attempt4Up',
+                [14, 15, 16, 15, 16, 17, 16, 17, 18, 17, 18, 19], 10, false);
+        this.dropAttemps.animations.add('attempt4Down',
+                [19, 18, 17, 18, 17, 16, 17, 16, 15, 16, 15, 14], 10, false);
+        this.dropAttemps.animations.add('attempt5Up',
+                [19, 20, 21, 20, 21, 22, 21, 22, 23, 22, 23, 24], 10, false);
+        this.dropAttemps.animations.add('attempt5Down',
+                [24, 23, 22, 21, 20, 19], 10, false);
+        this.dropAttemps.animations.add('attempt0', [0], 10, false);
+        this.dropAttemps.fixedToCamera = true;
     },
     decreaseLife: function() {
     	this.lifeCounter--;
     	this.lifeLabel.setText(this.lifeCounter);
+    },
+    increaseDropBar: function() {
+        this.dropCounter++;
+        if (this.dropCounter > 0 && this.dropCounter <= 5) {
+            this.dropAttemps.animations.play('attempt' + this.dropCounter +
+                    'Up');
+        }
+    },
+    decreaseDropBar: function() {
+        this.dropCounter--;
+        if (this.dropCounter > 0 && this.dropCounter <= 4) {
+            this.dropAttemps.animations.play('attempt' + this.dropCounter +
+                    'Down');
+        }
     },
 };
