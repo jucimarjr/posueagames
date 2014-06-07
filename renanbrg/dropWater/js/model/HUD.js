c3b0c387fe768d59a14b19bd5119e2e95323e13e
Bruno Campos <bruno.campos@openbossa.org>
2014-06-07 18:04:56 -0400

    Refactor in the HUD class
    Add a correct reference of the object game in the class HUD.

--- renanbrg/dropWater/js/model/HUD.js
+++ renanbrg/dropWater/js/model/HUD.js
@@ -51,11 +51,12 @@ HUD.prototype = {
         this.lifeAsset.fixedToCamera = true;
         
         var styleForX = { font: "26px Helvetica", fill: "#ffffff" };
-        this.xLabel = game.add.text(70, 26, "x", styleForX);
+        this.xLabel = this.game.add.text(70, 26, "x", styleForX);
         this.xLabel.fixedToCamera = true;
         
         var styleForLife = { font: "40px Helvetica", fill: "#ffffff" };
-        this.lifeLabel = game.add.text(90, 22, this.lifeCounter, styleForLife);
+        this.lifeLabel = this.game.add.text(90, 22, this.lifeCounter,
+                styleForLife);
         this.lifeLabel.fixedToCamera = true;
 
         this.dropAttemps = this.game.add.sprite(Config.global.screen.width - 60,
