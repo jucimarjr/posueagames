7bed1e4ffd4d082163907fe7ef67c75ceddddd84
josemdvdf <josemdvdf@gmail.com>
2014-06-10 10:53:43 -0400

    fase 9 e cipo

--- johnsilva/canoeirobarev2/js/states/GamePlay.js
+++ johnsilva/canoeirobarev2/js/states/GamePlay.js
@@ -2,22 +2,24 @@
 
 State.GamePlay = function (game) {
 	"use strict";
-	this.game = game;
-	var deadAnimation;
-	var jumping;
-	var cursors;
-	var levelConfig;
+	this.game = game;	
 };
 State.GamePlay.prototype = {
 	preload: function () {
-		
+		var deadAnimation;
+		var jumping;
+		var cursors;
+		var levelConfig;
+		var onCipo;
+		var frameClimbing;
 	},
 	create: function () {
 
+		frameClimbing = Config.climbing.frames.min;
 		this.game.physics.startSystem(Phaser.Game.ARCADE);
 		this.game.physics.arcade.gravity.y = 100;
 		this.game.stage.smoothed = false;
-		this.level = 1;
+		this.level = 9;
 
 		this.player = game.add.sprite(10,1000 ,'playerS');
 		this.player.anchor.setTo(.5, 1);		
@@ -28,7 +30,7 @@ State.GamePlay.prototype = {
 		//player.animations.add('walk',[1,2,1,3],12,true);
 		this.player.animations.add('walk',[3,4,5,6,7,8,9,10,11,12,13,14],20,true);
 		this.player.animations.add('stoped',[0,1],2,true);
-		this.player.animations.add('down',[3,4],12,true);		
+		this.player.animations.add('climbing',[20,21,22,23],8,false);		
 		this.player.animations.add('dead',[24,25,26],10,false);
 		/*deadAnimation.onComplete.add(function() {
 	    								this.loadLevel(this.level);
@@ -69,14 +71,11 @@ State.GamePlay.prototype = {
 
 		this.game.physics.arcade.collide(this.layer, this.player);
 		this.player.body.velocity.x = 0;
+		onCipo = false;
 
-	if(!this.gameOver){
-		
+	if(!this.gameOver){		
 		//Config.global.screen.resize(this.game);
 		//player.animations.play('walk');
-
-		
-
 		if(levelConfig.bees.id>0) this.game.physics.arcade.overlap(this.player, this.bees, this.collideWithBees, null,this);
 		if(levelConfig.thorns.id>0) this.game.physics.arcade.overlap(this.player, this.thorns, this.die, null,this);
 		//if(levelConfig.waters.id>0) this.game.physics.arcade.collide(this.player, this.acidicWater, this.die, null,this);
@@ -86,6 +85,9 @@ State.GamePlay.prototype = {
 		}
 		if(levelConfig.checkPoint.id>0){
 			this.game.physics.arcade.overlap(this.player, this.checkPoint, this.saveCP, null,this);
+		}
+		if(levelConfig.cipo.id>0){
+			this.game.physics.arcade.overlap(this.player, this.cipo, this.runCipo, null,this);
 		}
 
 		
@@ -126,7 +128,7 @@ State.GamePlay.prototype = {
 			//this.player.frame = 0;
 			this.player.animations.play('stoped');
 		}
-		if(this.player.body.velocity.y !== 0){
+		if(this.player.body.velocity.y !== 0 && !onCipo){
 			this.player.animations.play('jump');
 		}
 		if ( this.isToJumping() && (!jumping) ){
@@ -135,6 +137,9 @@ State.GamePlay.prototype = {
 			this.player.animations.play('jump');
 			this.player.body.velocity.y = -550;
 		}
+		if(onCipo && cursors.up.isDown){
+				this.climb();
+		}
 		if(!cursors.up.isDown){
 			jumping = false;
 		}
@@ -142,6 +147,8 @@ State.GamePlay.prototype = {
 	},
 
 	isToJumping: function(){
+		if(onCipo) return false;
+
 		if( cursors.up.isDown || this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) ){
 			if(this.player.body.onFloor()){
 				return true;
@@ -180,6 +187,7 @@ State.GamePlay.prototype = {
 		if (this.bushes) this.bushes.destroy();
 		if (this.acidicWater) this.acidicWater.destroy();
 		if (this.checkPoint) this.checkPoint.destroy();
+		if (this.cipo) this.cipo.destroy();
 
 		this.bg = this.game.add.tileSprite(0,0,1200,800,'bg'+level);
 		this.bg.fixedToCamera = true;		
@@ -193,6 +201,7 @@ State.GamePlay.prototype = {
 		
 		if(levelConfig.thorns.id>0) this.addThorns(levelConfig.thorns.id);
 		if(levelConfig.coin.id>0) this.addCoin(levelConfig.coin.id, levelConfig.coin.image);
+		if(levelConfig.cipo.id>0) this.addCipo(levelConfig.cipo.id);
 
 		this.player.bringToTop();
 
@@ -205,8 +214,17 @@ State.GamePlay.prototype = {
 		if(levelConfig.bees.id>0) this.addBees(levelConfig.bees.id);
 		if(levelConfig.tubes.id>0) this.addTubes(levelConfig.tubes.id);
 		if(levelConfig.checkPoint.id>0) this.addCheckPoint(levelConfig.checkPoint.id);
+	},
 
-
+	addCipo: function(id){
+		this.cipo = game.add.group();
+		this.cipo.enableBody = true;
+		this.cipo.physicsBodyType = Phaser.Physics.ARCADE;
+		this.map.createFromObjects('cipo',id,'cipo', 0,true,false,this.cipo);
+		this.cipo.forEach(function (c){ 
+			c.body.allowGravity = false;
+			c.body.immovable = true;
+		}, this);
 	},
 
 	addCheckPoint: function(id){
@@ -337,9 +355,28 @@ State.GamePlay.prototype = {
     	levelConfig.checkPoint.y = player.body.y;
     },
 
+    runCipo : function(player, cipo) {
+    	onCipo = true;
+    	player.body.velocity.y = -10;
+    	player.frame = Config.climbing.frames.min;
+    },
+
+    climb : function() {
+    	this.player.animations.play('climbing');
+    	this.player.body.velocity.y = -100;
+    	/*frameClimbing++;
+    	this.player.frame  = frameClimbing;
+    	if(frameClimbing > Config.climbing.frames.max){
+    		frameClimbing = Config.climbing.frames.min;
+    		this.player.frame = frameClimbing;
+    	}*/
+
+    },   
+
     render: function (){
     	//game.debug.body(this.player);
     	//game.debug.body(this.thorns);
+    	//game.debug.text(frameClimbing,32,32);
     	//game.debug.text(levelConfig.checkPoint.x,32,32);
 
 		/*this.thorns.forEach(function (thorn){ 
@@ -352,6 +389,10 @@ State.GamePlay.prototype = {
 
 		/*this.bees.forEach(function (bees){ 
 			game.debug.body(bees);
+		}, this);*/
+
+		/*this.cipo.forEach(function (c){ 
+			game.debug.body(c);
 		}, this);*/
     },
 };
