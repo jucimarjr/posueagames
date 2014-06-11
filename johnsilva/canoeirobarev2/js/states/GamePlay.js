345fb6a25d82c75f47c9631f7462b8590311616a
josemdvdf <josemdvdf@gmail.com>
2014-06-11 08:49:58 -0400

    code refactoring

--- johnsilva/canoeirobarev2/js/states/GamePlay.js
+++ johnsilva/canoeirobarev2/js/states/GamePlay.js
@@ -23,11 +23,6 @@ State.GamePlay.prototype = {
 
 		this.player = game.add.sprite(10,1000 ,'playerS');
 		this.player.anchor.setTo(.5, 1);		
-		//player.smoothed = false;
-		//player.body.checkCollision.up = false;
-		//player.body.checkCollision.left = false;
-		//player.body.checkCollision.right = false;
-		//player.animations.add('walk',[1,2,1,3],12,true);
 		this.player.animations.add('walk',[3,4,5,6,7,8,9,10,11,12,13,14],20,true);
 		this.player.animations.add('stoped',[0,1],2,true);
 		this.player.animations.add('climbing',[20,21,22,23],5,true);		
@@ -35,36 +30,14 @@ State.GamePlay.prototype = {
 		/*deadAnimation.onComplete.add(function() {
 	    								this.loadLevel(this.level);
 									}, this);	*/
-		jumpAnim = this.player.animations.add('jump',[15,16,17,18,19],9,false);
+		this.player.animations.add('jump',[15,16,17,18,19],9,false);
 		this.game.physics.enable(this.player);
 		this.player.body.collideWorldBounds = true;
 		this.game.physics.enable(this.player);
 		this.player.body.setSize(25, 60, 0, 0);
-		//this.player.body.bounce.set(0.5);
 		this.game.camera.y = 1000;
 		cursors = this.game.input.keyboard.createCursorKeys();
-		this.loadLevel(this.level);
-		
-		
-		
-		//jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-		/*this.right = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-		this.left = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-		this.down = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-		this.up = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-		this.spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-		this.right.onDown.add(function() {this.run=true;}, this);
-		this.right.onUp.add(function() {this.run=false;}, this);
-		this.left.onDown.add(function() {this.back=true;}, this);
-		this.left.onUp.add(function() {this.back=false;}, this);
-		this.down.onDown.add(function() {this.crouch=true;}, this);
-		this.down.onUp.add(function() {this.crouch=false;}, this);
-		this.up.onDown.add(function() {this.jump=true;}, this);
-		this.up.onUp.add(function() {this.jump=false;}, this);
-		this.spaceBar.onDown.add(function() {this.jump=true;}, this);
-		this.spaceBar.onUp.add(function() {this.jump=false;}, this);*/
+		this.loadLevel(this.level);		
 	},
 	update: function () {
 
@@ -72,82 +45,70 @@ State.GamePlay.prototype = {
 		this.player.body.velocity.x = 0;
 		onCipo = false;
 
-	if(!this.gameOver){		
-		//Config.global.screen.resize(this.game);
-		//player.animations.play('walk');
-		if(levelConfig.bees.id>0) this.game.physics.arcade.overlap(this.player, this.bees, this.collideWithBees, null,this);
-		if(levelConfig.thorns.id>0) this.game.physics.arcade.overlap(this.player, this.thorns, this.die, null,this);
-		//if(levelConfig.waters.id>0) this.game.physics.arcade.collide(this.player, this.acidicWater, this.die, null,this);
-		if(levelConfig.waters.id>0){
-			this.game.physics.arcade.overlap(this.player, this.waters, this.die, null,this);
-			this.game.physics.arcade.overlap(this.waters, this.layer, this.renew, null,this);
-		}
-		if(levelConfig.checkPoint.id>0){
-			this.game.physics.arcade.overlap(this.player, this.checkPoint, this.saveCP, null,this);
-		}
-		if(levelConfig.cipo.id>0){
-			this.game.physics.arcade.overlap(this.player, this.cipo, this.runCipo, null,this);
-		}
+		if(!this.gameOver){		
+			//Config.global.screen.resize(this.game);
+			if(levelConfig.bees.id>0) 
+				this.game.physics.arcade.overlap(this.player, this.bees, this.collideWithBees, null,this);
+			if(levelConfig.thorns.id>0) 
+				this.game.physics.arcade.overlap(this.player, this.thorns, this.die, null,this);
+			if(levelConfig.waters.id>0){
+				this.game.physics.arcade.overlap(this.player, this.waters, this.die, null,this);
+				this.game.physics.arcade.overlap(this.waters, this.layer, this.renew, null,this);
+			}
+			if(levelConfig.checkPoint.id>0){
+				this.game.physics.arcade.overlap(this.player, this.checkPoint, this.saveCP, null,this);
+			}
+			if(levelConfig.cipo.id>0){
+				this.game.physics.arcade.overlap(this.player, this.cipo, this.runCipo, null,this);
+			}		
+			this.game.physics.arcade.overlap(this.player, this.coin, function () {
+				levelConfig.checkPoint.x = 0;
+				levelConfig.checkPoint.y = 0;
+				this.loadLevel(this.level + 1);
+			}, null, this);		
+		
+        	if (cursors.left.isDown ) {
+				this.player.scale.x = 1; 
+				this.player.scale.x = -1;			
+				this.player.body.velocity.x = -Config.player.velocity.run;
+				this.crouched = false;
+				if(this.player.body.onFloor()){
+					this.player.animations.play('walk');
+				}
+			}
+			else if(cursors.right.isDown){
+				this.player.scale.x = -1; 
+				this.player.scale.x = 1;
+				this.player.body.velocity.x = Config.player.velocity.run;
+				this.crouched = false;
+				if(this.player.body.onFloor()){
+					this.player.animations.play('walk');
+				}
+			}
+			else if(this.player.body.blocked.down)
+				this.player.animations.play('stoped');
 
-		
-		this.game.physics.arcade.overlap(this.player, this.coin,
-			function () {
-			levelConfig.checkPoint.x = 0;
-			levelConfig.checkPoint.y = 0;
-			this.loadLevel(this.level + 1);
-			//this.loadLevel(3);
-		}, null, this);
-		
-		
-		//this.branches.tilePosition.x = this.player.body.x;
-        if (cursors.left.isDown ) {
-        //if (this.back){
-			this.player.scale.x = 1; 
-			this.player.scale.x = -1;			
-			this.player.body.velocity.x = -Config.player.velocity.run;
-			this.crouched = false;
-			if(this.player.body.onFloor()){
-				this.player.animations.play('walk');
+			if(this.player.body.velocity.y !== 0 && !onCipo){
+				//this.player.animations.play('jump');
+				this.player.frame = 19;
 			}
-			//this.bg.tilePosition.x+=1;
-		}
-		else if(cursors.right.isDown){
-		//else if(this.run){
-			this.player.scale.x = -1; 
-			this.player.scale.x = 1;
-			this.player.body.velocity.x = Config.player.velocity.run;
-			this.crouched = false;
-			if(this.player.body.onFloor()){
-				this.player.animations.play('walk');
+			if ( this.isToJumping() && (!jumping) ){
+			/*if (this.jump && this.player.body.onFloor()){	*/
+				jumping = true;
+				this.player.animations.play('jump');
+				this.player.body.velocity.y = -Config.player.velocity.jump;
+			}else if(onCipo){
+				if(cursors.up.isDown) 
+					this.climb(-Config.player.velocity.climbing);
+				else if(cursors.down.isDown) 
+					this.climb(Config.player.velocity.climbing);
+				else 
+					this.player.frame = Config.climbing.frames.max;
 			}
-			//this.bg.tilePosition.x-=1;
+			if(!cursors.up.isDown){
+				jumping = false;
+			}
 		}
-		else if(this.player.body.blocked.down){
-			//this.player.animations.stop();
-			//this.player.frame = 0;
-			this.player.animations.play('stoped');
-		}
-		if(this.player.body.velocity.y !== 0 && !onCipo){
-			//this.player.animations.play('jump');
-			this.player.frame = 19;
-		}
-		if ( this.isToJumping() && (!jumping) ){
-		/*if (this.jump && this.player.body.onFloor()){	*/
-			jumping = true;
-			this.player.animations.play('jump');
-			this.player.body.velocity.y = -Config.player.velocity.jump;
-		}else if(onCipo){
-			if(cursors.up.isDown) 
-				this.climb(-Config.player.velocity.climbing);
-			else if(cursors.down.isDown) 
-				this.climb(Config.player.velocity.climbing);
-			else 
-				this.player.frame = Config.climbing.frames.max;
-		}
-		if(!cursors.up.isDown){
-			jumping = false;
-		}
-	}
 	},
 
 	isToJumping: function(){
@@ -343,7 +304,6 @@ State.GamePlay.prototype = {
 		this.player.body.gravity.y = 0;
 		this.player.body.velocity.x = 0;
 		this.player.body.velocity.y = 0;
-		//this.player.anchor.setTo(0.5, 0.5);
 		this.player.animations.play('dead');
 		var t = this.game.add.tween(this.player).to({alpha:0}, 500).start();
 		enemie.kill();	
