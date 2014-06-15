48e5dc51acf76ddafb0324a7fe846fb31fcc5351
Marcos Castro <marcossilvadecastro@gmail.com>
2014-06-15 17:17:33 -0400

    inicio da mudanca do tipo arcade para p2js

--- halison/tracajet/js/states/Fase1.js
+++ halison/tracajet/js/states/Fase1.js
@@ -49,12 +49,18 @@ State.Fase1.prototype = {
 		var bg = game.add.tileSprite(0, 0, game.cache.getImage('bg').width,game.cache.getImage('bg').height, 'bg');
 
 		
-		game.physics.startSystem(Phaser.Game.ARCADE);
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		game.physics.p2.setImpactEvents(true);
+		game.physics.p2.restitution = 0.5;
+		game.physics.p2.gravity.y = 500;
+
 		this.map = game.add.tilemap('mapa'); //adicionando o map
 		this.map.addTilesetImage('assets_1','tilesetPlataforma' );// primeiro vem nome do arquivo, depois o apelido
 		
 		this.layer = this.map.createLayer('TileWorld');
 		this.layer.resizeWorld(); //seta o mundo com as alterações feitas
+		game.physics.p2.convertTilemap(this.map, this.layer);
+
 		//Colide com esses tilesets
 		this.map.setCollision([9,10,11,12,13,14,17,18,19,20,21,22], true,'TileWorld'); // 0 espaco vazio 1 em diante os tiles do tileset
 		this.map.setTileSize(36, 40);
@@ -66,14 +72,11 @@ State.Fase1.prototype = {
 		this.tracajet.animations.add('walk',[0,1,2,1],6,false);
 		this.tracajet.animations.add('swim',[5,6,7],6,false);
 		this.tracajet.animations.add('startSwim',[3,4],4,true);
-		game.physics.enable(this.tracajet, Phaser.Physics.ARCADE); // permite que a sprite tenha um corpo fisico
-		
-		this.tracajet.body.acceleration.y = 20;
+		game.physics.p2.enable(this.tracajet); // permite que a sprite tenha um corpo fisico
+		this.tracajet.body.setCircle(40);
+		this.tracajet.body.fixedRotation = true;
 		this.tracajet.body.collideWorldBounds = true;
-	    this.tracajet.body.drag.x = 200;
 		this.tracajet.anchor.setTo(.5,.5);
-	    this.tracajet.body.gravity.y = 50;
-		this.tracajet.body.setSize(38, 80);
 	    game.camera.follow(this.tracajet);
 
 	    //Group jacares
@@ -89,36 +92,35 @@ State.Fase1.prototype = {
 		this.map.createFromObjects(this.nameSheets,2,'folhas',0,true,false,this.sheets);
 
 		//Grupo chaves
-		this.keys = this.game.add.group();
-		this.keys.enableBody = true;
-		this.map.createFromObjects(this.nameKeys,1,'key_8080',0,true,false,this.keys);		
+		//this.keys = this.game.add.group();
+		//this.keys.enableBody = true;
+		//this.map.createFromObjects(this.nameKeys,1,'key_8080',0,true,false,this.keys);		
 		
+
+		//Score
+		//var moduloPositionX = Math.abs(this.game.world.position.x);
+		//var moduloPositionY = Math.abs(this.game.world.position.y); 
+		//this.txtScore = this.game.add.text(moduloPositionX  + this.game.width - 100,moduloPositionY + 20, "", {
+		//	font: "20px Arial",
+		//	fill: "#ff0044",
+		//	align: "left"
+		//});
+		//this.txtScore.setText("Score : " + Config.game.score.score);
+		
+		//this.game.input.keyboard.addCallbacks(this,this.changeGameState);
 
 		//Cursor
 		this.cursors = this.game.input.keyboard.createCursorKeys();
-
-
-		//Score
-		var moduloPositionX = Math.abs(this.game.world.position.x);
-		var moduloPositionY = Math.abs(this.game.world.position.y); 
-		this.txtScore = this.game.add.text(moduloPositionX  + this.game.width - 100,moduloPositionY + 20, "", {
-			font: "20px Arial",
-			fill: "#ff0044",
-			align: "left"
-		});
-		this.txtScore.setText("Score : " + Config.game.score.score);
-		
-		this.game.input.keyboard.addCallbacks(this,this.changeGameState);
 	},
 
 	update: function () {
-		game.physics.arcade.collide(this.tracajet, this.layer)
-	    game.physics.arcade.overlap(this.enemies, this.tracajet,this.gameOver, null,this);
-	    game.physics.arcade.overlap(this.sheets,this.tracajet,this.increaseScore,null,this);
-	    game.physics.arcade.overlap(this.keys,this.tracajet,this.increaseContKeys,null,this);
-	    this.updateTracajet();
-	    this.updateEnemies();
-	    this.updateScorePosition
+		//game.physics.p2.collide(this.tracajet, this.layer)
+	    //game.physics.p2.overlap(this.enemies, this.tracajet,this.gameOver, null,this);
+	   // game.physics.p2.overlap(this.sheets,this.tracajet,this.increaseScore,null,this);
+	   // game.physics.p2.overlap(this.keys,this.tracajet,this.increaseContKeys,null,this);
+	   	this.updateTracajet();
+	    //this.updateEnemies();
+	    //this.updateScorePosition
 		
 	},
 	increaseContKeys : function(tracajet,key){
@@ -148,7 +150,7 @@ State.Fase1.prototype = {
 		jacare = game.add.sprite(game.world.randomX, game.world.randomY, 'jacare');
 		jacare.animations.add('left',[0,1,2,3,4,5],10,true);
 		jacare.animations.add('right',[6,7,8,9,10,11],10,true);
-		game.physics.enable(jacare, Phaser.Physics.ARCADE); // permite que a sprite tenha um corpo fisico
+		game.physics.p2.enable(jacare); // permite que a sprite tenha um corpo fisico
 		
 		
 		jacare_tween = this.add.tween(jacare);
@@ -174,6 +176,7 @@ State.Fase1.prototype = {
 	,
 	updateTracajet : function(){
 		this.tracajet.body.velocity.x = 0;
+		this.tracajet.body.velocity.y = 0;
 
 		 if ( this.cursors.left.isDown) { // vai para esquerda
 	    	this.tracajet.body.velocity.x = -this.speed;
