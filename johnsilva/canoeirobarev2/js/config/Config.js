d08e858642630c077576575c482700810d1fbbfc
josemdvdf <josemdvdf@gmail.com>
2014-06-17 09:55:15 -0400

    flag phase 3

--- johnsilva/canoeirobarev2/js/config/Config.js
+++ johnsilva/canoeirobarev2/js/config/Config.js
@@ -200,7 +200,7 @@ Config.level = {
 		else if(level == 2)
 		    Config.update.updateLevel(65, 600, true, 13, 0, 18, 0, 22, 23, 'coinIara', 31,0,35);
 		else if(level == 3)
-		    Config.update.updateLevel(65, 600, true, 13, 18, 19, 23, 24, 25, 'coin', 33,0,0);
+		    Config.update.updateLevel(65, 600, true, 13, 18, 19, 23, 24, 25, 'coin', 33,0,37);
 		else if (level == 4)
 		    Config.update.updateLevel(75, 650, true, 13, 18, 19, 23, 24, 25, 'coinIara', 33,0,0);
 		else if (level == 5)
