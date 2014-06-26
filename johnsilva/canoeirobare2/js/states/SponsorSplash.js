e6e59f322c2b312421a89249b01e52f36aa641f0
DVD <josemdvdf@gmail.com>
2014-06-26 17:15:51 -0400

    sponsor

--- johnsilva/canoeirobare2/js/states/SponsorSplash.js
+++ johnsilva/canoeirobare2/js/states/SponsorSplash.js
@@ -12,13 +12,14 @@ State.SponsorSplash.prototype = {
 	},
 	create: function () {
 		"use strict";
-		var sprite = this.game.add.sprite(Config.sponsorSplash.x, Config.sponsorSplash.y, 'sponsor-splash');
+		/*var sprite = this.game.add.sprite(Config.sponsorSplash.x, Config.sponsorSplash.y, 'sponsor-splash');
 		setTimeout(function () {
 			this.game.add.tween(sprite).to({alpha : 0}, Config.sponsorSplash.millis, Phaser.Easing.Linear.None).start();
 		}, Config.sponsorSplash.millis);
 		setTimeout(function () {
 			this.game.state.start('GameSplash');
-		}, Config.sponsorSplash.nextState);
+		}, Config.sponsorSplash.nextState);*/
+		this.game.state.start('GameSplash');
 	},
 	update: function () {
 		"use strict";
