23d8784f4c859e944d1cdffb65c8e09fd1578d98
Andrediasdelima <andredias_m@hotmail.com>
2014-06-29 10:35:00 -0400

    alterações preload

--- renanbrg/dropstory/js/states/Level1Preloader.js
+++ renanbrg/dropstory/js/states/Level1Preloader.js
@@ -42,8 +42,8 @@ State.Level1Preloader.prototype = {
                 'assets/images/strawtipright_15-20.png');
         this.game.load.image('smoke', './assets/images/smoke_32-32.png');
         this.game.load.audio('jump','assets/waterDrop.mp3');
-        this.game.load.audio('main','assets/gotaMain.wav');
-        this.game.load.audio('powup','assets/gotaPowerUp.wav');
+        this.game.load.audio('main','assets/gotaMain.mp3');
+        this.game.load.audio('powup','assets/gotaPowerUp.mp3');
     },
     create: function() {
         "use strict";
