6da78e5ab63e79422834e3ea548cd0cf6d4309fa
Raymundo Junior <ext-raymundo.junior@microsoft.com>
2014-06-30 13:50:14 -0400

    adicionando texto credito

--- brunorichele/platform/js/states/Credits.js
+++ brunorichele/platform/js/states/Credits.js
@@ -12,6 +12,22 @@ State.Credits.prototype = {
 		var background = this.game.add.sprite(Config.credits.x, Config.credits.y, 'credits');
 		var button = this.game.add.button(Config.menu.buttonBack.x, Config.menu.buttonBack.y, 'button-back', this.onBack, this, 1, 0, 1, 0);
 		button.anchor.setTo(Config.menu.buttonBack.anchor.x, Config.menu.buttonBack.anchor.y);
+		
+		var style = { font: "40px Helvetica", fill: "#ffffff" };
+		game.add.text(880, 200, 'Créditos', style);
+        game.add.text(590, 300, 'Universidade do Estado do Amazonas', style);
+        game.add.text(650, 350, 'Escola Superior de Tecnologia', style);	
+		game.add.text(450, 400, 'Especialização em Desenvolvimento de Jogos Eletrônicos', style);
+		
+        game.add.text(880, 500, 'Design', style);
+		game.add.text(830, 550, 'Bruno Richele', style);	
+		
+        game.add.text(850, 650, 'Programação', style);
+		game.add.text(860, 700, 'Diego Nobu', style);
+		game.add.text(830, 750, 'Raymundo Junior', style);
+		
+		game.add.text(850, 850, 'Orientação', style);
+		game.add.text(650, 900, 'Prof.Dr. Jucimar Maia da Silva Jr', style);								
 	},
 	update: function () {
 		"use strict";
