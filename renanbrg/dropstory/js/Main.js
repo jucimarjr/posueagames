72b7b34e17832b39e1ae42253facaf44db1fddd6
Bruno Campos <bruno.campos@openbossa.org>
2014-07-01 09:49:48 -0400

    Fix the game restart
    - Now the game over state is correctly called if the player loses all the
    available lives;
    - Change the game over state to make a smoother transition to the menu
    state.

--- renanbrg/dropstory/js/Main.js
+++ renanbrg/dropstory/js/Main.js
@@ -4,12 +4,12 @@ var game = new Phaser.Game(Config.global.screen.width,
 game.state.add('ludus-state', State.LudusSplash);
 game.state.add('sponsor-state', State.SponsorSplash);
 game.state.add('pause-state', State.Pause);
+game.state.add('gameover-state', State.GameOver);
 game.state.add('level1preloader-state', State.Level1Preloader);
 game.state.add('level1-state', State.Level1);
 game.state.add('menu-state', State.Menu);
 game.state.add('howtoplay-state', State.HowToPlay);
 game.state.add('credits-state', State.Credits);
 game.state.add('boot-state', State.Boot);
-game.state.add('gameOver-state', State.GameOver);
 
-game.state.start('ludus-state');
+game.state.start('boot-state');
