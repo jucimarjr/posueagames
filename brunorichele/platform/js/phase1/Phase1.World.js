0f36ffcd1d46c65fe3bc20c05a13ae583ae5382c
Raymundo Junior <ext-raymundo.junior@microsoft.com>
2014-07-03 13:32:06 -0400

    alterando a gravidade y do mundo

--- brunorichele/platform/js/phase1/Phase1.World.js
+++ brunorichele/platform/js/phase1/Phase1.World.js
@@ -21,7 +21,7 @@ Phase1.World = {
 	createBg : function(){
 		this.game.world.setBounds(0, 0, this.weith, this.height);
 		this.game.physics.startSystem(Phaser.Physics.P2JS);
-		this.game.physics.p2.gravity.y = 850;
+		this.game.physics.p2.gravity.y = 1400;
 		this.game.physics.p2.restitution = 0;
 		
 		game.physics.p2.setImpactEvents(true);
